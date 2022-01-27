9a34e75d84ddc0417ff743b37fd2050e3b0ace9b
Ildar Galeev <KeinAsylum@gmail.com>
2022-01-27 13:23:25 +0300

    Simplify ThriftMetaLoader

--- src/app/api/thrift-instance-provider/thrift-meta-loader.service.ts
+++ src/app/api/thrift-instance-provider/thrift-meta-loader.service.ts
@@ -1,37 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 import { ThriftAstMetadata } from '../../thrift-services';
-import { ApiMetadata } from './types/api-metadata';
 
 @Injectable({ providedIn: 'root' })
 export class ThriftMetaLoader {
-    private metadata$: BehaviorSubject<ApiMetadata> = new BehaviorSubject({});
+    private requests: { [name: string]: Observable<ThriftAstMetadata[]> } = {};
 
     constructor(private http: HttpClient) {}
 
     get(name: string): Observable<ThriftAstMetadata[]> {
-        return this.metadata$.pipe(
-            switchMap((meta) => {
-                const foundedName = Object.keys(meta).find((metaName) => metaName === name);
-                if (foundedName) {
-                    return of(meta[foundedName]);
-                }
-                return this.load(name);
-            })
-        );
-    }
-
-    private load(name: string): Observable<ThriftAstMetadata[]> {
-        return this.http.get<ThriftAstMetadata[]>(`assets/api-meta/${name}.json`).pipe(
-            tap((meta) =>
-                this.metadata$.next({
-                    ...this.metadata$.value,
-                    [name]: meta,
-                })
-            )
-        );
+        const req = this.requests[name];
+        return req
+            ? req
+            : (this.requests[name] = this.http
+                  .get<ThriftAstMetadata[]>(`assets/api-meta/${name}.json`)
+                  .pipe(shareReplay(1)));
     }
 }
