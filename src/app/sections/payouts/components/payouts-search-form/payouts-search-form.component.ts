14a05b44f1e7f955b66985c13a9d2bff3d233eb6
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-03-01 16:11:42 +0300

    prepare search form

--- src/app/sections/payouts/components/payouts-search-form/payouts-search-form.component.ts
+++ src/app/sections/payouts/components/payouts-search-form/payouts-search-form.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@ngneat/reactive-forms';
 
 @Component({
     selector: 'cc-payouts-search-form',
     templateUrl: './payouts-search-form.component.html',
 })
-export class PayoutsSearchFormComponent {}
+export class PayoutsSearchFormComponent {
+    form = this.fb.group({ payoutId: null, merchant: null });
+
+    constructor(private fb: FormBuilder) {}
+}
--- src/app/sections/payouts/payouts.module.ts
+++ src/app/sections/payouts/payouts.module.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
 
+import { MerchantFieldModule } from '@cc/app/shared/components/merchant-field';
 import { EmptySearchResultModule } from '@cc/components/empty-search-result';
 
 import { PayoutsSearchFormComponent } from './components/payouts-search-form/payouts-search-form.component';
@@ -18,10 +23,16 @@ import { PayoutsComponent } from './payouts.component';
         CommonModule,
         PayoutsRoutingModule,
         MatButtonModule,
-        FlexModule,
         MatCardModule,
         MatProgressBarModule,
         EmptySearchResultModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        MerchantFieldModule,
+        FlexLayoutModule,
+        ReactiveFormsModule,
+        MatSelectModule,
     ],
 })
 export class PayoutsModule {}
