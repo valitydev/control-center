cafcb520e62a16de0752575b3e005167a43d73ca
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-03-22 14:19:03 +0300

    fix

--- src/app/thrift-services/fistful/repairer.service.ts
+++ src/app/thrift-services/fistful/repairer.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import * as Repairer from '../../api/fistful/withdrawal-session/gen-nodejs/Repairer';
+import { RepairScenario as RepairScenarioObject } from '../../api/fistful/withdrawal-session/gen-nodejs/withdrawal_session_types';
 import { KeycloakTokenInfoService } from '../../keycloak-token-info.service';
 import { ThriftService } from '../services/thrift/thrift-service';
 import { RepairScenario, SessionID } from './gen-model/withdrawal_session';
-import * as Repairer from './gen-nodejs/Repairer';
-import { RepairScenario as RepairScenarioObject } from './gen-nodejs/withdrawal_session_types';
 
 @Injectable()
 export class RepairerService extends ThriftService {
