fcc15e9d46dcbe910f2b641ab0095fbdb55b85a7
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-03-25 00:13:41 +0300

    remove RevertManagementService

--- src/app/thrift-services/fistful/fistful.module.ts
+++ src/app/thrift-services/fistful/fistful.module.ts
@@ -3,14 +3,8 @@ import { NgModule } from '@angular/core';
 import { FistfulAdminService } from './fistful-admin.service';
 import { FistfulStatisticsService } from './fistful-stat.service';
 import { RepairerService } from './repairer.service';
-import { RevertManagementService } from './revert-management.service';
 
 @NgModule({
-    providers: [
-        RepairerService,
-        FistfulAdminService,
-        RevertManagementService,
-        FistfulStatisticsService,
-    ],
+    providers: [RepairerService, FistfulAdminService, FistfulStatisticsService],
 })
 export class FistfulModule {}
