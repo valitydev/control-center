8e760cfb46bd22c546c10e209291c6689f2b958a
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-03-25 12:42:07 +0300

    add typings

--- src/app/api/payment-processing/services/user-info/user-info.service.ts
+++ src/app/api/payment-processing/services/user-info/user-info.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { UserInfo } from '@vality/domain-proto/lib/payment_processing';
+import { Observable } from 'rxjs';
 import { first, map, shareReplay } from 'rxjs/operators';
 
 import { KeycloakTokenInfoService } from '@cc/app/shared/services';
@@ -7,7 +9,7 @@ import { KeycloakTokenInfoService } from '@cc/app/shared/services';
     providedIn: 'root',
 })
 export class UserInfoService {
-    userInfo$ = this.keycloakTokenInfoService.decoded$.pipe(
+    userInfo$: Observable<UserInfo> = this.keycloakTokenInfoService.decoded$.pipe(
         map(({ sub }) => ({
             id: sub,
             type: { internal_user: {} },
