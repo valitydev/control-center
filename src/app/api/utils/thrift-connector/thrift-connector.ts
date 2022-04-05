04ba47cbbd1a5368f69f152a30a61ad34c04bf64
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-04-05 17:30:47 +0300

    add http error handling

--- src/app/api/utils/thrift-connector/thrift-connector.ts
+++ src/app/api/utils/thrift-connector/thrift-connector.ts
@@ -1,45 +1,65 @@
+import connectClient from '@vality/woody';
+import isNil from 'lodash-es/isNil';
 import { Observable } from 'rxjs';
-import { switchMap, shareReplay, map, first } from 'rxjs/operators';
+import { switchMap, first } from 'rxjs/operators';
 
 import { KeycloakTokenInfoService } from '@cc/app/shared/services';
 
-import {
-    connectToThriftService,
-    prepareThriftServiceMethod,
-    toConnectOptions,
-    ThriftService,
-    ThriftServiceConnection,
-} from './utils';
+import { toConnectOptions, ThriftService } from './utils';
 
 export class ThriftConnector {
-    private connection$: Observable<ThriftServiceConnection>;
-
     constructor(
         protected keycloakTokenInfoService: KeycloakTokenInfoService,
         protected service: ThriftService,
         protected endpoint: string,
-        deprecatedHeaders = false
-    ) {
-        this.connection$ = this.keycloakTokenInfoService.decoded$.pipe(
-            map((token) => toConnectOptions(token, deprecatedHeaders)),
-            switchMap((connectOptions) =>
-                connectToThriftService(endpoint, service, connectOptions)
-            ),
-            shareReplay({
-                bufferSize: 1,
-                refCount: true,
-            })
-        );
-    }
+        protected deprecatedHeaders = false
+    ) {}
 
     protected callThriftServiceMethod<T>(
         serviceMethodName: string,
         ...args: unknown[]
     ): Observable<T> {
-        return this.connection$.pipe(
+        return this.keycloakTokenInfoService.decoded$.pipe(
             first(),
-            map((connection) => prepareThriftServiceMethod<T>(connection, serviceMethodName)),
-            switchMap((fn) => fn(...args))
+            switchMap(
+                (token) =>
+                    new Observable<T>((observer) => {
+                        try {
+                            /**
+                             * Connection errors come with HTTP errors (!= 200) and should be handled with errors from the service.
+                             * You need to have 1 free connection per request. Otherwise, the error cannot be caught or identified.
+                             * TODO: Optimization option: add a connection pool.
+                             */
+                            const connection = connectClient(
+                                location.hostname,
+                                location.port,
+                                this.endpoint,
+                                this.service,
+                                toConnectOptions(token, this.deprecatedHeaders),
+                                (err) => {
+                                    observer.error(err);
+                                    observer.complete();
+                                }
+                            );
+                            const serviceMethod = connection[serviceMethodName];
+                            if (isNil(serviceMethod)) {
+                                observer.error(
+                                    `Service method: "${serviceMethodName}" is not found in thrift client`
+                                );
+                                observer.complete();
+                            } else {
+                                serviceMethod.bind(connection)(...args, (err, result) => {
+                                    if (err) observer.error(err);
+                                    else observer.next(result);
+                                    observer.complete();
+                                });
+                            }
+                        } catch (err) {
+                            observer.error(err);
+                            observer.complete();
+                        }
+                    })
+            )
         );
     }
 }
