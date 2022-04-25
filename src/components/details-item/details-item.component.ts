7b8afd1c900ddb6051d45bccae1ce576e315b500
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-04-25 19:56:31 +0300

    upd details item

--- src/components/details-item/details-item.component.ts
+++ src/components/details-item/details-item.component.ts
@@ -1,11 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { ContentObserver } from '@angular/cdk/observers';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { defer, ReplaySubject, switchMap } from 'rxjs';
+import { delay, distinctUntilChanged, map, share, startWith } from 'rxjs/operators';
 
 @Component({
     selector: 'cc-details-item',
     templateUrl: 'details-item.component.html',
     styleUrls: ['details-item.component.scss'],
 })
-export class DetailsItemComponent {
-    @Input()
-    title: string;
+export class DetailsItemComponent implements AfterViewInit {
+    @Input() title: string;
+
+    @ViewChild('content') contentElementRef: ElementRef<HTMLElement>;
+
+    isEmpty$ = defer(() => this.viewInit$).pipe(
+        switchMap(() =>
+            this.contentObserver.observe(this.contentElementRef.nativeElement).pipe(startWith(null))
+        ),
+        map(() => this.getIsEmpty()),
+        distinctUntilChanged(),
+        delay(0),
+        share()
+    );
+
+    private viewInit$ = new ReplaySubject<void>(1);
+
+    constructor(private contentObserver: ContentObserver) {}
+
+    ngAfterViewInit() {
+        this.viewInit$.next();
+    }
+
+    private getIsEmpty() {
+        return !Array.from(this.contentElementRef.nativeElement.childNodes).find(
+            (n) =>
+                n.nodeType !== Node.COMMENT_NODE ||
+                (n.nodeType === Node.TEXT_NODE && n.nodeValue.trim())
+        );
+    }
 }
