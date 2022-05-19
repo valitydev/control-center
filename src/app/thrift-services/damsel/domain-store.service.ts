76500c6004b012b38e81f253e64e88c2078bfeb7
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-05-19 16:30:50 +0300

    ref domain store

--- src/app/thrift-services/damsel/domain-store.service.ts
+++ src/app/thrift-services/damsel/domain-store.service.ts
@@ -1,81 +1,62 @@
 import { Injectable } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { Domain, DomainObject } from '@vality/domain-proto/lib/domain';
+import { DomainObject } from '@vality/domain-proto/lib/domain';
 import { Commit, Snapshot, Version } from '@vality/domain-proto/lib/domain_config';
-import { BehaviorSubject, Observable, of, ReplaySubject } from 'rxjs';
+import { BehaviorSubject, defer, Observable, of, ReplaySubject } from 'rxjs';
 import { map, pluck, share, shareReplay, startWith, switchMap, take, tap } from 'rxjs/operators';
 
+import { RepositoryService } from '@cc/app/api/domain-config';
+import { inProgressFrom, progressTo } from '@cc/utils';
 import { getUnionKey } from '@cc/utils/get-union-key';
-
-import { toGenReference } from '../converters';
-import { DomainService } from './domain.service';
-import { createDamselInstance, damselInstanceToObject } from './utils/create-damsel-instance';
 
 @UntilDestroy()
 @Injectable()
 export class DomainStoreService {
-    /**
-     * @deprecated use domain$
-     */
-    domain: Observable<Domain>;
-    snapshot$: Observable<Snapshot>;
-    domain$: Observable<Domain>;
-    version$: Observable<number>;
-    get isLoading$(): Observable<boolean> {
-        return this._isLoading$.asObservable();
-    }
-
-    private reload$ = new ReplaySubject<void>(1);
-    private snapshot = this.reload$.pipe(
+    snapshot$: Observable<Snapshot> = defer(() => this.reload$).pipe(
         startWith(undefined),
-        tap(() => this._isLoading$.next(true)),
-        switchMap(() => this.dmtService.checkout(toGenReference())),
-        tap(() => this._isLoading$.next(false)),
+        switchMap(() =>
+            this.repositoryService.Checkout({ head: {} }).pipe(progressTo(this.progress$))
+        ),
+        untilDestroyed(this),
         shareReplay(1)
     );
-    private _isLoading$ = new BehaviorSubject(true);
+    domain$ = this.snapshot$.pipe(pluck('domain'), share());
+    version$ = this.snapshot$.pipe(pluck('version'), share());
+    isLoading$ = inProgressFrom(() => this.progress$, this.snapshot$);
 
-    constructor(private dmtService: DomainService) {
-        this.domain$ = this.snapshot.pipe(pluck('domain'), shareReplay(1));
-        this.snapshot$ = this.snapshot.pipe(
-            map((s) => damselInstanceToObject('domain_config', 'Snapshot', s)),
-            shareReplay(1)
-        );
-        this.domain$ = this.snapshot$.pipe(pluck('domain'));
-        this.version$ = this.snapshot$.pipe(pluck('version')) as Observable<any>;
+    private reload$ = new ReplaySubject<void>(1);
+    private progress$ = new BehaviorSubject(0);
 
-        this.snapshot.pipe(untilDestroyed(this)).subscribe();
-    }
+    constructor(private repositoryService: RepositoryService) {}
 
     forceReload(): void {
         this.reload$.next();
     }
 
-    getObjects = <T extends keyof DomainObject>(objectType: T): Observable<DomainObject[T][]> =>
-        this.domain$.pipe(
+    getObjects<T extends keyof DomainObject>(objectType: T): Observable<DomainObject[T][]> {
+        return this.domain$.pipe(
             map((d) =>
                 Array.from(d.values())
                     .filter((o) => getUnionKey(o) === objectType)
                     .map((o) => o[objectType])
             )
         );
+    }
 
-    commit = (commit: Commit, version?: Version | number, reload = true) => {
+    commit(commit: Commit, version?: Version | number, reload = true) {
         const version$ = version ? of(version) : this.version$.pipe(take(1));
         return version$.pipe(
-            switchMap((v) =>
-                this.dmtService.commit(
-                    createDamselInstance('domain_config', 'Version', v),
-                    createDamselInstance('domain_config', 'Commit', commit)
-                )
-            ),
-            map((v) => damselInstanceToObject('domain_config', 'Version', v)),
-            tap(() => reload && this.forceReload()),
-            share()
+            switchMap((v) => this.repositoryService.Commit(v, commit)),
+            tap(() => {
+                if (reload) this.forceReload();
+            })
         );
-    };
+    }
 
-    sequenceCommits([commit, ...otherCommits]: Commit[], version?: Version | number) {
+    sequenceCommits(
+        [commit, ...otherCommits]: Commit[],
+        version?: Version | number
+    ): Observable<number> {
         return otherCommits.length
             ? this.commit(commit, version, false).pipe(
                   switchMap((v) => this.sequenceCommits(otherCommits, v))
