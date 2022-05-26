189dbea732eaac2d3e63ed85a9d9d5382df6a0a0
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-05-26 04:07:22 +0300

    upd predicate

--- src/app/sections/payment-routing-rules/shop-payment-routing-ruleset/add-shop-payment-routing-rule-dialog/predicate/predicate.component.ts
+++ src/app/sections/payment-routing-rules/shop-payment-routing-ruleset/add-shop-payment-routing-rule-dialog/predicate/predicate.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, Injector, OnChanges } from '@angular/core';
 import { ValidationErrors, Validator } from '@angular/forms';
 import { WrappedFormControlSuperclass } from '@s-libs/ng-core';
 import { Predicate } from '@vality/domain-proto/lib/domain';
 import { from } from 'rxjs';
 
+import { DomainMetadataFormExtensionsService } from '@cc/app/shared/services';
 import { createControlProviders } from '@cc/utils';
 
 @Component({
@@ -16,6 +17,14 @@ export class PredicateComponent
     implements Validator, OnChanges
 {
     metadata$ = from(import('@vality/domain-proto/lib/metadata.json').then((m) => m.default));
+    extensions$ = this.domainMetadataFormExtensionsService.metadataFormExtensions$;
+
+    constructor(
+        injector: Injector,
+        private domainMetadataFormExtensionsService: DomainMetadataFormExtensionsService
+    ) {
+        super(injector);
+    }
 
     validate(): ValidationErrors | null {
         return this.control.invalid ? this.control.errors || { controlsInvalid: true } : null;
