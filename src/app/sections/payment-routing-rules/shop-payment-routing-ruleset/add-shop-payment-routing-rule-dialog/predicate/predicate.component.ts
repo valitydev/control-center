244f572fd2aa56f1d3b4f698ca677aeaf49e390d
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-05-28 01:10:50 +0300

    fix

--- src/app/sections/payment-routing-rules/shop-payment-routing-ruleset/add-shop-payment-routing-rule-dialog/predicate/predicate.component.ts
+++ src/app/sections/payment-routing-rules/shop-payment-routing-ruleset/add-shop-payment-routing-rule-dialog/predicate/predicate.component.ts
@@ -1,11 +1,9 @@
 import { Component, Injector, OnChanges } from '@angular/core';
-import { ValidationErrors, Validator } from '@angular/forms';
-import { WrappedFormControlSuperclass } from '@s-libs/ng-core';
 import { Predicate } from '@vality/domain-proto/lib/domain';
 import { from } from 'rxjs';
 
 import { DomainMetadataFormExtensionsService } from '@cc/app/shared/services';
-import { createControlProviders } from '@cc/utils';
+import { createControlProviders, ValidatedFormControlSuperclass } from '@cc/utils';
 
 @Component({
     selector: 'cc-predicate',
@@ -13,8 +11,8 @@ import { createControlProviders } from '@cc/utils';
     providers: createControlProviders(PredicateComponent),
 })
 export class PredicateComponent
-    extends WrappedFormControlSuperclass<Predicate>
-    implements Validator, OnChanges
+    extends ValidatedFormControlSuperclass<Predicate>
+    implements OnChanges
 {
     metadata$ = from(import('@vality/domain-proto/lib/metadata.json').then((m) => m.default));
     extensions$ = this.domainMetadataFormExtensionsService.extensions$;
@@ -24,9 +22,5 @@ export class PredicateComponent
         private domainMetadataFormExtensionsService: DomainMetadataFormExtensionsService
     ) {
         super(injector);
-    }
-
-    validate(): ValidationErrors | null {
-        return this.control.invalid ? this.control.errors || { controlsInvalid: true } : null;
     }
 }
