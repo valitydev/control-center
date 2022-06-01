0867824e5725874955c5587340a4577d9c57bc25
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-06-01 15:16:06 +0300

    rename

--- src/app/sections/routing-rules/shop-payment-routing-ruleset/shop-payment-routing-ruleset.component.ts
+++ src/app/sections/routing-rules/shop-payment-routing-ruleset/shop-payment-routing-ruleset.component.ts
@@ -69,16 +69,12 @@ export class ShopPaymentRoutingRulesetComponent {
                 next: (res) => {
                     if (res.status === BaseDialogResponseStatus.Success) {
                         this.domainStoreService.forceReload();
-                        this.notificationService.success(
-                            'Shop payment routing ruleset successfully added'
-                        );
+                        this.notificationService.success('Shop routing ruleset successfully added');
                     }
                 },
                 error: (err) => {
                     this.errorService.error(err);
-                    this.notificationService.success(
-                        'Error while adding shop payment routing ruleset'
-                    );
+                    this.notificationService.success('Error while adding shop routing ruleset');
                 },
             });
     }
