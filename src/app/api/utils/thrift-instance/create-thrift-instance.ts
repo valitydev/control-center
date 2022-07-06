ddd23bcca6a238128db56aec45f7a56dc3e47c5f
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-06 12:07:11 +0300

    support include

--- src/app/api/utils/thrift-instance/create-thrift-instance.ts
+++ src/app/api/utils/thrift-instance/create-thrift-instance.ts
@@ -1,4 +1,4 @@
-import type { Field, ValueType } from '@vality/thrift-ts';
+import type { Field, JsonAST, ValueType } from '@vality/thrift-ts';
 import Int64 from '@vality/thrift-ts/lib/int64';
 
 import {
@@ -15,30 +15,31 @@ export function createThriftInstance<V>(
     instanceContext: ThriftInstanceContext,
     namespaceName: string,
     indefiniteType: ValueType,
-    value: V
+    value: V,
+    include?: JsonAST['include']
 ): V {
     if (isThriftObject(value)) {
         return value;
     }
     const { namespace, type } = parseNamespaceType(indefiniteType, namespaceName);
-    const internalCreateThriftInstance = (t: ValueType, v: V) =>
-        createThriftInstance(metadata, instanceContext, namespace, t, v);
+    const internalCreateThriftInstance = (t: ValueType, v: V, include: JsonAST['include']) =>
+        createThriftInstance(metadata, instanceContext, namespace, t, v, include);
     if (isComplexType(type)) {
         switch (type.name) {
             case 'map':
                 return new Map(
                     Array.from(value as unknown as Map<any, any>).map(([k, v]) => [
-                        internalCreateThriftInstance(type.keyType, k),
-                        internalCreateThriftInstance(type.valueType, v),
+                        internalCreateThriftInstance(type.keyType, k, include),
+                        internalCreateThriftInstance(type.valueType, v, include),
                     ])
                 ) as unknown as V;
             case 'list':
                 return (value as unknown as any[]).map((v) =>
-                    internalCreateThriftInstance(type.valueType, v)
+                    internalCreateThriftInstance(type.valueType, v, include)
                 ) as unknown as V;
             case 'set':
                 return Array.from(value as unknown as Set<any>).map((v) =>
-                    internalCreateThriftInstance(type.valueType, v)
+                    internalCreateThriftInstance(type.valueType, v, include)
                 ) as unknown as V;
             default:
                 throw new Error('Unknown complex thrift type');
@@ -51,7 +52,11 @@ export function createThriftInstance<V>(
                 return value;
         }
     }
-    const { namespaceMetadata, objectType } = parseNamespaceObjectType(metadata, namespace, type);
+    const {
+        namespaceMetadata,
+        objectType,
+        include: objectInclude,
+    } = parseNamespaceObjectType(metadata, namespace, type, include);
     switch (objectType) {
         case 'enum':
             return value;
@@ -62,13 +67,17 @@ export function createThriftInstance<V>(
             try {
                 if (objectType === 'typedef') {
                     const typedefMeta = (typeMeta as { type: ValueType }).type;
-                    return internalCreateThriftInstance(typedefMeta, value);
+                    return internalCreateThriftInstance(typedefMeta, value, objectInclude);
                 }
                 const instance = new instanceContext[namespace][type]();
                 for (const [k, v] of Object.entries(value)) {
                     type StructOrUnionType = Field[];
                     const fieldTypeMeta = (typeMeta as StructOrUnionType).find((m) => m.name === k);
-                    instance[k] = internalCreateThriftInstance(fieldTypeMeta.type, v);
+                    instance[k] = internalCreateThriftInstance(
+                        fieldTypeMeta.type,
+                        v,
+                        objectInclude
+                    );
                 }
                 return instance;
             } catch (error) {
