fa3059c665f9ba993ad3a0c55bf6df08b6f4567c
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-14 16:18:06 +0300

    init table

--- src/app/sections/repairing/repairing.component.ts
+++ src/app/sections/repairing/repairing.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { RepairStatus } from '@vality/repairer-proto';
 
-import { RepairManagementService } from '../../api/repairer';
+import { MachinesService } from './services/machines.service';
 
 @Component({
     selector: 'cc-repairing',
     templateUrl: './repairing.component.html',
+    providers: [MachinesService],
 })
 export class RepairingComponent {
-    machines$ = this.repairManagementService.Search({ limit: 10 });
+    machines$ = this.machinesService.searchResult$;
+    inProgress$ = this.machinesService.doAction$;
+    hasMore$ = this.machinesService.hasMore$;
+    filters = this.fb.group({});
+    selection: any;
+    displayedColumns = [
+        'id',
+        'namespace',
+        'createdAt',
+        'status',
+        'provider',
+        'errorMessage',
+        'history',
+    ];
+    statuses = Object.fromEntries(Object.entries(RepairStatus).map(([k, v]) => [v, k]));
 
-    constructor(private repairManagementService: RepairManagementService) {}
+    constructor(private machinesService: MachinesService, private fb: FormBuilder) {
+        this.machinesService.search({});
+    }
+
+    repair() {}
+
+    fetchMore() {
+        this.machinesService.fetchMore();
+    }
 }
--- src/app/sections/repairing/repairing.module.ts
+++ src/app/sections/repairing/repairing.module.ts
@@ -1,10 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
+import { EmptySearchResultModule } from '../../../components/empty-search-result';
+import { TableModule } from '../../../components/table';
+import { RepairingRoutingModule } from './repairing-routing.module';
 import { RepairingComponent } from './repairing.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        RepairingRoutingModule,
+        TableModule,
+        MatCardModule,
+        ReactiveFormsModule,
+        FlexLayoutModule,
+        MatProgressBarModule,
+        MatButtonModule,
+        EmptySearchResultModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatBadgeModule,
+    ],
     declarations: [RepairingComponent],
 })
 export class RepairingModule {}
