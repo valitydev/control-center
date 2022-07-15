d9f15e6e3c762ba4e522cc747da8e8cb77d033d1
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-15 14:27:31 +0300

    add filters

--- src/app/sections/repairing/repairing.component.ts
+++ src/app/sections/repairing/repairing.component.ts
@@ -1,33 +1,92 @@
-import { Component } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { RepairStatus } from '@vality/repairer-proto';
+import { DateRange } from '@angular/material/datepicker';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Machine, Namespace, ProviderID, RepairStatus } from '@vality/repairer-proto';
+import isEmpty from 'lodash-es/isEmpty';
+import isNil from 'lodash-es/isNil';
+import omitBy from 'lodash-es/omitBy';
+import { Moment } from 'moment';
+import { map } from 'rxjs/operators';
 
+import { getEnumKeys } from '../../../utils';
+import { QueryParamsService } from '../../shared/services';
 import { MachinesService } from './services/machines.service';
 
+interface Filters {
+    ids: string;
+    ns: Namespace;
+    timespan: DateRange<Moment>;
+    provider_id: ProviderID;
+    status: RepairStatus;
+    error_message: string;
+}
+
+@UntilDestroy()
 @Component({
     selector: 'cc-repairing',
     templateUrl: './repairing.component.html',
     providers: [MachinesService],
 })
-export class RepairingComponent {
+export class RepairingComponent implements OnInit {
     machines$ = this.machinesService.searchResult$;
     inProgress$ = this.machinesService.doAction$;
     hasMore$ = this.machinesService.hasMore$;
-    filters = this.fb.group({});
-    selection: any;
-    displayedColumns = [
-        'id',
-        'namespace',
-        'createdAt',
-        'status',
-        'provider',
-        'errorMessage',
-        'history',
-    ];
-    statuses = Object.fromEntries(Object.entries(RepairStatus).map(([k, v]) => [v, k]));
+    filters = this.fb.group<Filters>({
+        ids: null,
+        ns: null,
+        timespan: null,
+        provider_id: null,
+        status: null,
+        error_message: null,
+    });
+    selection: SelectionModel<Machine>;
+    displayedColumns = ['id', 'namespace', 'createdAt', 'provider', 'status', 'history'];
+    statusNameByValue = Object.fromEntries(Object.entries(RepairStatus).map(([k, v]) => [v, k]));
+    status = RepairStatus;
+    statuses = getEnumKeys(RepairStatus);
 
-    constructor(private machinesService: MachinesService, private fb: FormBuilder) {
-        this.machinesService.search({});
+    constructor(
+        private machinesService: MachinesService,
+        private fb: FormBuilder,
+        private qp: QueryParamsService<Filters>
+    ) {}
+
+    ngOnInit() {
+        this.filters.valueChanges
+            .pipe(
+                map(() => {
+                    return omitBy(this.filters.value, isEmpty);
+                }),
+                untilDestroyed(this)
+            )
+            .subscribe((v: Filters) => this.qp.set(v));
+        this.qp.params$
+            .pipe(
+                map(({ ids, ns, timespan: d, provider_id, status, error_message }) => {
+                    const timespan = omitBy(
+                        {
+                            from_time: d?.start?.toISOString(),
+                            to_time: d?.end?.toISOString(),
+                        },
+                        isNil
+                    );
+                    return omitBy(
+                        {
+                            ids: ids?.split(/[,.;\s]/)?.filter(Boolean),
+                            ns,
+                            provider_id,
+                            status,
+                            error_message,
+                            timespan: Object.keys(timespan).length ? timespan : null,
+                        },
+                        isEmpty
+                    );
+                }),
+                untilDestroyed(this)
+            )
+            .subscribe((params) => this.machinesService.search(params));
     }
 
     repair() {}
--- src/app/sections/repairing/repairing.module.ts
+++ src/app/sections/repairing/repairing.module.ts
@@ -5,12 +5,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { EmptySearchResultModule } from '../../../components/empty-search-result';
 import { TableModule } from '../../../components/table';
+import { DateRangeModule } from '../../shared/components/date-range/date-range.module';
 import { RepairingRoutingModule } from './repairing-routing.module';
 import { RepairingComponent } from './repairing.component';
 
@@ -28,6 +32,10 @@ import { RepairingComponent } from './repairing.component';
         MatTableModule,
         MatTooltipModule,
         MatBadgeModule,
+        MatFormFieldModule,
+        MatInputModule,
+        DateRangeModule,
+        MatSelectModule,
     ],
     declarations: [RepairingComponent],
 })
