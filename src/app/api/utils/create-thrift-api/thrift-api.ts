d8c13852f31df0d1a90b919ceba41bfc82bc3da2
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-20 00:24:24 +0300

    remove ThriftConnector

--- src/app/api/utils/create-thrift-api/thrift-api.ts
+++ src/app/api/utils/create-thrift-api/thrift-api.ts
@@ -1,28 +1,41 @@
-import { from } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { KeycloakService } from 'keycloak-angular';
+import { combineLatest, from, Observable } from 'rxjs';
+import { first, map, switchMap } from 'rxjs/operators';
 
-import { ThriftConnector } from '../thrift-connector';
+import { KeycloakTokenInfoService } from '../../../shared/services';
 import { createThriftInstance, thriftInstanceToObject } from '../thrift-instance';
 import { ThriftApiArgs } from './types/thrift-api-args';
+import { ThriftApiOptions } from './types/thrift-api-options';
+import { callThriftServiceMethod, toConnectOptions } from './utils';
 
-export class ThriftApi extends ThriftConnector {
+export class ThriftApi {
+    options: ThriftApiOptions;
+    keycloakTokenInfoService: KeycloakTokenInfoService;
+    keycloakService: KeycloakService;
+
     constructor(...[injector, options]: ThriftApiArgs) {
-        super(injector, options);
+        this.keycloakTokenInfoService = injector.get(KeycloakTokenInfoService);
+        this.keycloakService = injector.get(KeycloakService);
+        this.options = options;
+        this.initServiceMethods();
+    }
+
+    private initServiceMethods() {
         Object.assign(
             this,
             Object.fromEntries(
-                options.functions.map((methodName) => [
+                this.options.functions.map((methodName) => [
                     methodName,
                     (...methodArgs) =>
-                        from(options.metadata()).pipe(
+                        from(this.options.metadata()).pipe(
                             switchMap((metadata) =>
                                 this.callThriftServiceMethodWithConvert(
-                                    options.name,
-                                    options.serviceName,
+                                    this.options.name,
+                                    this.options.serviceName,
                                     methodName,
                                     methodArgs,
                                     metadata,
-                                    options.context
+                                    this.options.context
                                 )
                             )
                         ),
@@ -53,6 +66,36 @@ export class ThriftApi extends ThriftConnector {
         );
         return this.callThriftServiceMethod(methodName, ...methodThriftArgs).pipe(
             map((v) => thriftInstanceToObject(metadata, namespaceName, methodMetadata.type, v))
+        );
+    }
+
+    private callThriftServiceMethod<T>(
+        serviceMethodName: string,
+        ...args: unknown[]
+    ): Observable<T> {
+        return combineLatest([
+            this.keycloakTokenInfoService.decoded$,
+            this.keycloakService.getToken(),
+        ]).pipe(
+            first(),
+            switchMap(([parsedToken, token]) =>
+                callThriftServiceMethod<T>(
+                    {
+                        hostname: this.options.hostname,
+                        port: this.options.port,
+                        service: this.options.service,
+                        path: this.options.path,
+                        ...toConnectOptions(
+                            parsedToken,
+                            this.options.wachterServiceName,
+                            token,
+                            this.options.deprecatedHeaders
+                        ),
+                    },
+                    serviceMethodName,
+                    args
+                )
+            )
         );
     }
 }
