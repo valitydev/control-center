ef095dd4f02a13a39f6099be4291c9993d43f3cf
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-08-08 22:15:27 +0300

    partyId required

--- src/app/sections/payment-adjustment/search-form/search-form.component.ts
+++ src/app/sections/payment-adjustment/search-form/search-form.component.ts
@@ -34,7 +34,7 @@ export class SearchFormComponent implements OnInit {
             fromTime: [moment(), Validators.required],
             toTime: [moment(), Validators.required],
             invoiceIds: '',
-            partyId: '',
+            partyId: ['', Validators.required],
             shopId: '',
             fromRevision: [0, Validators.required],
             toRevision: ['', Validators.required],
