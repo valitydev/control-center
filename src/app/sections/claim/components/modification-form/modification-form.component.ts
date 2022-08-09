a70ba9ee9b74ab86dc41a0ba5c51c46b56d0d227
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-08-09 21:24:12 +0300

    fix extensions$

--- src/app/sections/claim/components/modification-form/modification-form.component.ts
+++ src/app/sections/claim/components/modification-form/modification-form.component.ts
@@ -2,10 +2,10 @@ import { Component, Injector, Input, OnChanges } from '@angular/core';
 import { Validator } from '@angular/forms';
 import { Claim } from '@vality/domain-proto/lib/claim_management';
 import { Party } from '@vality/domain-proto/lib/domain';
-import { from, Observable } from 'rxjs';
+import { from, combineLatest, ReplaySubject, defer } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { ComponentChanges, MetadataFormExtension } from '@cc/app/shared';
+import { ComponentChanges } from '@cc/app/shared';
 import { DomainMetadataFormExtensionsService } from '@cc/app/shared/services/domain-metadata-form-extensions';
 import { createControlProviders, ValidatedFormControlSuperclass } from '@cc/utils';
 
@@ -25,7 +25,14 @@ export class ModificationFormComponent
     @Input() type: string;
 
     metadata$ = from(import('@vality/domain-proto/lib/metadata.json').then((m) => m.default));
-    extensions$: Observable<MetadataFormExtension[]>;
+    extensions$ = combineLatest([
+        defer(() => this.claimOrPartyChanged$).pipe(
+            map(() => createPartyClaimMetadataFormExtensions(this.party, this.claim))
+        ),
+        this.domainMetadataFormExtensionsService.extensions$,
+    ]).pipe(map((extensionGroups) => extensionGroups.flat()));
+
+    private claimOrPartyChanged$ = new ReplaySubject<void>(1);
 
     constructor(
         injector: Injector,
@@ -37,12 +44,7 @@ export class ModificationFormComponent
     ngOnChanges(changes: ComponentChanges<ModificationFormComponent>) {
         super.ngOnChanges(changes);
         if (changes.party || changes.claim) {
-            this.extensions$ = this.domainMetadataFormExtensionsService.extensions$.pipe(
-                map((e) => [
-                    ...createPartyClaimMetadataFormExtensions(this.party, this.claim),
-                    ...e,
-                ])
-            );
+            this.claimOrPartyChanged$.next();
         }
     }
 }
