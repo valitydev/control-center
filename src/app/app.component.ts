d2587f640d3fc70d9d080685f1f6c47a1381b036
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-08-23 12:27:12 +0300

    Repairing (deprecated)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,6 +58,11 @@ export class AppComponent implements OnInit {
             { name: 'Merchants', route: '/parties', activateRoles: [PartyRole.Get] },
             {
                 name: 'Repairing',
+                route: '/old-repairing',
+                activateRoles: [DomainConfigRole.Checkout],
+            },
+            {
+                name: 'New repairing',
                 route: '/repairing',
                 activateRoles: [DomainConfigRole.Checkout],
             },
