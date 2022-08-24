d233050c86461ce7a1cb821e529dacfa6100a6ef
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-08-24 21:27:30 +0300

    clear

--- src/app/domain/utils.ts
+++ src/app/domain/utils.ts
@@ -1,15 +1,6 @@
-import { Reference } from '@vality/domain-proto/lib/domain';
 import * as uuid from 'uuid/v4';
 
 import { MonacoFile } from '../monaco-editor';
-
-export function parseRef(ref: string): Reference {
-    try {
-        return JSON.parse(ref) as Reference;
-    } catch {
-        throw new Error('Malformed domain object ref');
-    }
-}
 
 export const toMonacoFile = (content: string): MonacoFile => ({
     uri: `${uuid()}.json`,
