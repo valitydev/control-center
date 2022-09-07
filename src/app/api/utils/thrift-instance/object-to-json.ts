f368c3d5ab49aa9dd3b7131b08c7a5ca3c5dd8b7
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-09-07 14:19:47 +0300

    fix converter

--- src/app/api/utils/thrift-instance/object-to-json.ts
+++ src/app/api/utils/thrift-instance/object-to-json.ts
@@ -1,6 +1,8 @@
-export function objectToJSON(obj: any): any {
+export function objectToJSON(obj: unknown): unknown {
     if (typeof obj !== 'object' || obj === null) {
         return obj;
+    } else if (Array.isArray(obj)) {
+        return obj.map((v) => objectToJSON(v));
     } else if (obj instanceof Map) {
         return Array.from(obj).map(([k, v]) => [objectToJSON(k), objectToJSON(v)]);
     } else if (obj instanceof Set) {
