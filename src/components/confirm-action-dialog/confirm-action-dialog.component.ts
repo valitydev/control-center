2b79ed327753bf4895f0ebcac59ab912efcc6609
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-09-14 15:22:37 +0300

    suspend and blocking

--- src/components/confirm-action-dialog/confirm-action-dialog.component.ts
+++ src/components/confirm-action-dialog/confirm-action-dialog.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { BaseDialogResponseStatus, BaseDialogSuperclass } from '@vality/ng-core';
 
 @Component({
@@ -8,13 +9,22 @@ import { BaseDialogResponseStatus, BaseDialogSuperclass } from '@vality/ng-core'
 })
 export class ConfirmActionDialogComponent extends BaseDialogSuperclass<
     ConfirmActionDialogComponent,
-    { title?: string; confirmLabel?: string } | void
+    { title?: string; confirmLabel?: string; hasReason?: boolean } | void,
+    { reason?: string }
 > {
+    control = new FormControl<string>('');
+
     cancel() {
         this.dialogRef.close({ status: BaseDialogResponseStatus.Cancelled });
     }
 
     confirm() {
-        this.dialogRef.close({ status: BaseDialogResponseStatus.Success });
+        this.dialogRef.close({
+            status: BaseDialogResponseStatus.Success,
+            data:
+                this.dialogData && this.dialogData.hasReason
+                    ? { reason: this.control.value }
+                    : null,
+        });
     }
 }
