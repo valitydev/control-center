cef66b53e5fceb7966612841a47ccfb74b2d4664
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-09-14 15:31:20 +0300

    unused

--- src/app/shared/utils/index.ts
+++ src/app/shared/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './extract-claim-status';
 export * from './component-changes';
 export * from './polling-conditions';
-export * from './sort-units';
 export * from './deposit-status';
 export * from './is-empty-value';
