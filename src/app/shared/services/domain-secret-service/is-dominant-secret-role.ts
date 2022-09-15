3ae8438e1b88882bf2cc0f46205a67fd6b83ede7
Ildar Galeev <KeinAsylum@gmail.com>
2022-09-15 16:50:35 +0300

    Disable dmt secret role

--- src/app/shared/services/domain-secret-service/is-dominant-secret-role.ts
+++ src/app/shared/services/domain-secret-service/is-dominant-secret-role.ts
@@ -1,6 +1,5 @@
-import isNil from 'lodash-es/isNil';
+// export const isDominantSecretRole = (keycloakRoles: string[], secretRole: string): boolean =>
+//     !isNil(keycloakRoles.find((role) => role === secretRole)) || !environment.production;
 
-import { environment } from '@cc/environments/environment';
-
-export const isDominantSecretRole = (keycloakRoles: string[], secretRole: string): boolean =>
-    !isNil(keycloakRoles.find((role) => role === secretRole)) || !environment.production;
+export const isDominantSecretRole = (_keycloakRoles: string[], _secretRole: string): boolean =>
+    true;
