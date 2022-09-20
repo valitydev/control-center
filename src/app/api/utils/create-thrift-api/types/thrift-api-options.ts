f75e07797c09d384aa2e3d9b2522869894f4c852
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-09-21 01:59:22 +0300

    refactor thrift api calls

--- src/app/api/utils/create-thrift-api/types/thrift-api-options.ts
+++ src/app/api/utils/create-thrift-api/types/thrift-api-options.ts
@@ -1,12 +1,15 @@
-export interface ThriftApiOptions {
+import { Overwrite } from 'utility-types';
+
+import { ThriftInstanceContext } from '../../thrift-instance';
+import { ThriftClientMainOptions } from '../utils';
+
+export interface ThriftApiOptions extends Overwrite<ThriftClientMainOptions, { path?: string }> {
     name: string;
-    service: object;
     serviceName: string;
-    path: string;
-    hostname?: string;
-    port?: string;
-    metadata: () => Promise<any>;
-    context: any;
+    // https://github.com/valitydev/wachter/blob/master/src/main/resources/application.yml
+    wachterServiceName?: string;
+    metadata: () => Promise<unknown>;
+    context: ThriftInstanceContext;
     functions: string[];
     deprecatedHeaders?: boolean;
 }
