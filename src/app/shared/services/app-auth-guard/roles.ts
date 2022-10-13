328a24cbf3f0a655adbbf6ba0d3ef4e149ce1f85
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-10-13 18:39:10 +0600

    clean

--- src/app/shared/services/app-auth-guard/roles.ts
+++ src/app/shared/services/app-auth-guard/roles.ts
@@ -1,8 +1,3 @@
-// Unused:
-// accounting_report:get,
-// internal_report:get
-export enum ReportRole {}
-
 export enum PaymentAdjustmentRole {
     Create = 'adjustment:create',
 }
@@ -11,18 +6,10 @@ export enum OperationRole {
     SearchOperations = 'search_ops',
     SearchPayments = 'search_payments',
     SearchDeposits = 'search_deposits',
-    SearchInvoices = 'search_invoices',
 }
 
 export enum PayoutRole {
     Read = 'payout:read',
-    Confirm = 'payout:confirm',
-    Generate = 'payout:generate',
-    Cancel = 'payout:cancel',
-    Pay = 'payout:pay',
-    // Unused:
-    // payout:accept_abs,
-    // payout:accept_ones,
 }
 
 export enum PartyRole {
@@ -35,29 +22,13 @@ export enum DepositRole {
 
 export enum DomainConfigRole {
     Checkout = 'dmt:checkout',
-    // Unused:
-    // dmt:pull,
-    // dmt:commit'
 }
 
 export enum ClaimManagementRole {
     GetClaims = 'get_claims',
-    AddClaimMod = 'add_claim_mod',
-    AddPartyMod = 'add_party_mod',
     RequestClaimReview = 'request_claim_review',
     RequestClaimChanges = 'request_claim_changes',
     DenyClaim = 'deny_claim',
     RevokeClaim = 'revoke_claim',
     AcceptClaim = 'accept_claim',
-    UpdateClaim = 'update_claim',
-    CreateClaim = 'create_claim',
-    // Unused:
-    // claim:update
-    // claim.action:get
-    // claim.comment:add,
-    // claim.comment:get,
-    // claim:get,
-    // claim:accept
-    // merchant:create
-    // merchant:update
 }
