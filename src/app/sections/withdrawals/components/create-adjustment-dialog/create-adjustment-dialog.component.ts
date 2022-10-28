12a328cddc97450fea1b53b89e5f881cfd05989b
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-10-28 22:27:34 +0600

    upd form

--- src/app/sections/withdrawals/components/create-adjustment-dialog/create-adjustment-dialog.component.ts
+++ src/app/sections/withdrawals/components/create-adjustment-dialog/create-adjustment-dialog.component.ts
@@ -3,8 +3,8 @@ import { Validators } from '@angular/forms';
 import { FormControl } from '@ngneat/reactive-forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { ExternalID } from '@vality/fistful-proto/lib/base';
+import { ChangeRequest } from '@vality/fistful-proto/lib/deposit_adjustment';
 import { StatWithdrawal } from '@vality/fistful-proto/lib/fistful_stat';
-import { Status } from '@vality/fistful-proto/lib/withdrawal';
 import { BaseDialogResponseStatus, BaseDialogSuperclass } from '@vality/ng-core';
 import { combineLatest, from, of } from 'rxjs';
 import { catchError, finalize } from 'rxjs/operators';
@@ -24,19 +24,23 @@ export class CreateAdjustmentDialogComponent extends BaseDialogSuperclass<
     CreateAdjustmentDialogComponent,
     { withdrawals: StatWithdrawal[] }
 > {
-    statusControl = new FormControl<Status>(
-        { failed: { failure: { code: 'account_limit_exceeded:unknown' } } },
+    control = new FormControl<ChangeRequest>(
+        {
+            change_status: {
+                new_status: { failed: { failure: { code: 'account_limit_exceeded:unknown' } } },
+            },
+        },
         [Validators.required]
     );
     externalIdControl = new FormControl<ExternalID>();
-    typeControl = new FormControl<number>(0);
-    metadata$ = from(import('@vality/fistful-proto/lib/metadata.json').then((m) => m.default));
-    extensions: MetadataFormExtension[] = [
+    externalIdExtensions: MetadataFormExtension[] = [
         {
             determinant: () => of(true),
             extension: () => of({ label: 'External ID' }),
         },
     ];
+    typeControl = new FormControl<number>(0);
+    metadata$ = from(import('@vality/fistful-proto/lib/metadata.json').then((m) => m.default));
     progress = -1;
 
     constructor(
@@ -55,7 +59,7 @@ export class CreateAdjustmentDialogComponent extends BaseDialogSuperclass<
                 this.managementService
                     .CreateAdjustment(w.id, {
                         id: this.typeControl.value === 0 ? w.id : short().uuid(),
-                        change: { change_status: { new_status: this.statusControl.value } },
+                        change: this.control.value,
                         external_id: this.externalIdControl.value,
                     })
                     .pipe(
