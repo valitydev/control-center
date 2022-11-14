c45d9041c554add5de5f63068e96f0a3867114ca
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-11-14 20:08:00 +0600

    timeline to new json viewer

--- src/app/sections/claim/components/shop-modification-timeline-item/shop-modification-timeline-item.component.ts
+++ src/app/sections/claim/components/shop-modification-timeline-item/shop-modification-timeline-item.component.ts
@@ -1,13 +1,8 @@
 import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { Claim, ModificationUnit } from '@vality/domain-proto/lib/claim_management';
-import { Category } from '@vality/dominant-cache-proto';
-import { combineLatest, defer, of, ReplaySubject } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
 
-import { DominantCacheService } from '@cc/app/api/dominant-cache';
 import { ComponentChanges } from '@cc/app/shared';
-import { NotificationService } from '@cc/app/shared/services/notification';
-import { getUnionKey } from '@cc/utils';
 
 @Component({
     selector: 'cc-shop-modification-timeline-item',
@@ -18,38 +13,7 @@ export class ShopModificationTimelineItemComponent implements OnChanges {
     @Input() claim: Claim;
     @Output() claimChanged = new EventEmitter<void>();
 
-    extended$ = combineLatest([
-        defer(() => this.modificationUnit$),
-        this.dominantCacheService.GetCategories().pipe(
-            catchError((err) => {
-                this.notificationService.error('Categories were not loaded');
-                console.error(err);
-                return of([] as Category[]);
-            })
-        ),
-    ]).pipe(
-        map(([modificationUnit, categories]) => {
-            const modification =
-                modificationUnit.modification.party_modification.shop_modification.modification;
-            switch (getUnionKey(modification)) {
-                case 'creation': {
-                    const category = categories.find(
-                        (c) => c.ref === String(modification.creation?.category?.id)
-                    );
-                    return [{ path: ['category', 'id'], value: category?.name }];
-                }
-                default:
-                    return [];
-            }
-        })
-    );
-
     private modificationUnit$ = new ReplaySubject<ModificationUnit>(1);
-
-    constructor(
-        private dominantCacheService: DominantCacheService,
-        private notificationService: NotificationService
-    ) {}
 
     ngOnChanges({ modificationUnit }: ComponentChanges<ShopModificationTimelineItemComponent>) {
         if (modificationUnit) {
