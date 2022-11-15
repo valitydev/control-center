90177d624ae583d5373d85a90b82c9d3a0748210
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-11-15 17:30:04 +0600

    fix KeyComponent

--- src/app/shared/components/json-viewer/components/key/key.component.ts
+++ src/app/shared/components/json-viewer/components/key/key.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { of, switchMap } from 'rxjs';
+import { Component, Input, OnChanges } from '@angular/core';
+import { of, switchMap, ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { ComponentChanges } from '@cc/app/shared';
 
 import { MetadataViewItem } from '../../utils/metadata-view';
 
@@ -9,17 +11,28 @@ import { MetadataViewItem } from '../../utils/metadata-view';
     templateUrl: './key.component.html',
     styleUrls: ['./key.component.scss'],
 })
-export class KeyComponent {
+export class KeyComponent implements OnChanges {
     @Input() keys?: MetadataViewItem[];
+    keys$ = new ReplaySubject<MetadataViewItem[]>(1);
+    numberKey$ = this.keys$.pipe(
+        switchMap((keys) => {
+            if (keys.length !== 1) return of(null);
+            return this.keys[0].key$.pipe(
+                switchMap((key) => key.renderValue$),
+                map((value) => {
+                    if (typeof value === 'number') return `${value + 1}`;
+                    return null;
+                })
+            );
+        })
+    );
 
-    get numberKey$() {
-        if (this.keys.length > 1) return of(null);
-        return this.keys[0].key$.pipe(
-            switchMap((key) => key.renderValue$),
-            map((value) => {
-                if (typeof value === 'number') return `${value + 1}`;
-                return null;
-            })
-        );
+    ngOnChanges(changes: ComponentChanges<KeyComponent>) {
+        if (changes.keys) this.keys$.next(this.keys);
+    }
+
+    isUnion(pathItem: MetadataViewItem) {
+        if (!pathItem?.data$) return of(false);
+        return pathItem.data$.pipe(map((data) => data?.trueParent?.objectType === 'union'));
     }
 }
