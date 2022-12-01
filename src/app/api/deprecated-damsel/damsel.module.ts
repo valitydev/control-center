52ccbb247d9ce2909f9e32cdf70270e7471a5f53
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-12-01 15:44:57 +0600

    rm old paym procc serv

--- src/app/api/deprecated-damsel/damsel.module.ts
+++ src/app/api/deprecated-damsel/damsel.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { PaymentProcessingService } from './payment-processing.service';
 import { RoutingRulesModule } from './routing-rules';
 
 @NgModule({
     imports: [RoutingRulesModule],
-    providers: [PaymentProcessingService],
 })
 /**
  * @deprecated
--- src/app/api/deprecated-damsel/index.ts
+++ src/app/api/deprecated-damsel/index.ts
@@ -2,4 +2,3 @@ export * from './utils/create-damsel-instance';
 export * from './damsel.module';
 export * from './routing-rules';
 export * from './domain-store.service';
-export * from './payment-processing.service';
