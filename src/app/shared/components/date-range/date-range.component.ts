d47050a02f4509a0e76cb1472e558c8097984ff6
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-12-15 22:28:13 +0600

    fix payouts search

--- src/app/shared/components/date-range/date-range.component.ts
+++ src/app/shared/components/date-range/date-range.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ValidationErrors } from '@angular/forms';
 import { DateRange } from '@angular/material/datepicker';
 import { Moment } from 'moment';
 
@@ -19,5 +19,14 @@ export class DateRangeComponent extends ValidatedControlSuperclass<DateRange<Mom
 
     constructor(private fb: FormBuilder) {
         super();
+    }
+
+    validate(): ValidationErrors | null {
+        return (
+            super.validate() ??
+            (!this.control.value.start || !this.control.value.end
+                ? { oneOfTheDatesIsEmpty: true }
+                : null)
+        );
     }
 }
