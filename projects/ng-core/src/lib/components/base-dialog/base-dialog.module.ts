99a116bddeae05c11c5d6d5a278b3b0bfafdcb83
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-12-16 16:39:06 +0600

    fix

--- projects/ng-core/src/lib/components/base-dialog/base-dialog.module.ts
+++ projects/ng-core/src/lib/components/base-dialog/base-dialog.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
 import { MatLegacyProgressBarModule as MatProgressBarModule } from '@angular/material/legacy-progress-bar';
 
 import { ActionsModule } from '../actions';
