d538fe68eb5db155dcc3c545552795715efa068f
Ildar Galeev <KeinAsylum@gmail.com>
2022-12-20 22:19:14 +0300

    Remove file storage proto

--- src/app/sections/claim/services/upload-file.service.ts
+++ src/app/sections/claim/services/upload-file.service.ts
@@ -1,59 +1,48 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BaseDialogResponseStatus, BaseDialogService } from '@vality/ng-core';
-import moment from 'moment';
-import { switchMap } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 
-import { ClaimManagementService } from '@cc/app/api/claim-management';
-import { FileStorageService } from '@cc/app/api/file-storage';
-import { NotificationService } from '@cc/app/shared/services/notification';
 import { ConfirmActionDialogComponent } from '@cc/components/confirm-action-dialog';
 
 @Injectable()
 export class UploadFileService {
-    constructor(
-        private notificationService: NotificationService,
-        private baseDialogService: BaseDialogService,
-        private fileStorageService: FileStorageService,
-        private http: HttpClient,
-        private claimManagementService: ClaimManagementService
-    ) {}
+    constructor(private baseDialogService: BaseDialogService) {}
 
-    upload(file: File, partyId: string, claimId: number, revision: number) {
+    upload(_file: File, _partyId: string, _claimId: number, _revision: number) {
         return this.baseDialogService
             .open(ConfirmActionDialogComponent, { title: 'Upload file' })
             .afterClosed()
             .pipe(
-                filter(({ status }) => status === BaseDialogResponseStatus.Success),
-                switchMap(() =>
-                    this.fileStorageService.CreateNewFile(
-                        new Map(),
-                        moment().add(1, 'h').toISOString()
-                    )
-                ),
-                switchMap((data) =>
-                    this.http
-                        .put(data.upload_url, file, {
-                            headers: {
-                                'Content-Disposition': [
-                                    'attachment',
-                                    `filename=${encodeURI(file.name)}`,
-                                ].join(';'),
-                                'Content-Type': '',
-                            },
-                        })
-                        .pipe(map(() => data.file_data_id))
-                ),
-                switchMap((id) =>
-                    this.claimManagementService.UpdateClaim(partyId, claimId, revision, [
-                        {
-                            claim_modification: {
-                                file_modification: { id, modification: { creation: {} } },
-                            },
-                        },
-                    ])
-                )
+                filter(({ status }) => status === BaseDialogResponseStatus.Success)
+                // TODO it's not available to bump https://github.com/valitydev/file-storage-proto to codegen v2 now.
+                // switchMap(() =>
+                //     this.fileStorageService.CreateNewFile(
+                //         new Map(),
+                //         moment().add(1, 'h').toISOString()
+                //     )
+                // ),
+                // switchMap((data) =>
+                //     this.http
+                //         .put(data.upload_url, file, {
+                //             headers: {
+                //                 'Content-Disposition': [
+                //                     'attachment',
+                //                     `filename=${encodeURI(file.name)}`,
+                //                 ].join(';'),
+                //                 'Content-Type': '',
+                //             },
+                //         })
+                //         .pipe(map(() => data.file_data_id))
+                // ),
+                // switchMap((id) =>
+                //     this.claimManagementService.UpdateClaim(partyId, claimId, revision, [
+                //         {
+                //             claim_modification: {
+                //                 file_modification: { id, modification: { creation: {} } },
+                //             },
+                //         },
+                //     ])
+                // )
             );
     }
 }
