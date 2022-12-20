62521c067f53c358a23f7d8d94af34cb6ea69d0d
Ildar Galeev <KeinAsylum@gmail.com>
2022-12-20 20:46:52 +0300

    Bump to valitydev/dominant-cache-proto@9001474

--- src/app/api/dominant-cache/dominant-cache.service.ts
+++ src/app/api/dominant-cache/dominant-cache.service.ts
@@ -1,23 +1,47 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
-    codegenClientConfig,
-    CodegenClient,
-} from '@vality/dominant-cache-proto/lib/dominant_cache-DominantCache';
-import context from '@vality/dominant-cache-proto/lib/dominant_cache/context';
-import * as DominantCache from '@vality/dominant-cache-proto/lib/dominant_cache/gen-nodejs/DominantCache';
+    ThriftAstMetadata,
+    DominantCacheCodegenClient,
+    DominantCache,
+} from '@vality/dominant-cache-proto';
+import { Category, ContractTemplate } from '@vality/dominant-cache-proto/dominant_cache';
+import { combineLatest, from, map, Observable, switchMap } from 'rxjs';
 
-import { createThriftApi } from '@cc/app/api/utils';
+import { KeycloakTokenInfoService, toWachterHeaders } from '@cc/app/shared/services';
+import { environment } from '@cc/environments/environment';
 
 @Injectable({ providedIn: 'root' })
-export class DominantCacheService extends createThriftApi<CodegenClient>() {
-    constructor(injector: Injector) {
-        super(injector, {
-            service: DominantCache,
-            wachterServiceName: 'DominantCache',
-            metadata: () =>
-                import('@vality/dominant-cache-proto/lib/metadata.json').then((m) => m.default),
-            context,
-            ...codegenClientConfig,
-        });
+export class DominantCacheService {
+    private client$: Observable<DominantCacheCodegenClient>;
+
+    constructor(private keycloakTokenInfoService: KeycloakTokenInfoService) {
+        const headers$ = this.keycloakTokenInfoService.decoded$.pipe(
+            map(toWachterHeaders('DominantCache'))
+        );
+        const metadata$ = from(
+            import('@vality/dominant-cache-proto/metadata.json').then(
+                (m) => m.default as ThriftAstMetadata[]
+            )
+        );
+        this.client$ = combineLatest([metadata$, headers$]).pipe(
+            switchMap(([metadata, headers]) =>
+                DominantCache({
+                    metadata,
+                    headers,
+                    logging: environment.logging.requests,
+                    path: '/wachter',
+                })
+            )
+        );
+    }
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    GetCategories(): Observable<Category[]> {
+        return this.client$.pipe(switchMap((c) => c.GetCategories()));
+    }
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    GetContractTemplates(): Observable<ContractTemplate[]> {
+        return this.client$.pipe(switchMap((c) => c.GetContractTemplates()));
     }
 }
