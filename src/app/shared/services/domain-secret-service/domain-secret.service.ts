c6c137463e98e28938cb2202b083b91e9a78a4be
Ildar Galeev <KeinAsylum@gmail.com>
2023-03-21 17:54:43 +0700

    Fix restoreDomain obj changes

--- src/app/shared/services/domain-secret-service/domain-secret.service.ts
+++ src/app/shared/services/domain-secret-service/domain-secret.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Domain, DomainObject } from '@vality/domain-proto/domain';
 import { KeycloakService } from 'keycloak-angular';
+import cloneDeep from 'lodash-es/cloneDeep';
 import isNil from 'lodash-es/isNil';
 
 import { isDominantSecretRole } from './is-dominant-secret-role';
@@ -38,10 +39,13 @@ export class DomainSecretService {
         if (this.isDominantSecret) {
             return newObject;
         }
+        let result = newObject;
         const found = EXCLUDE_OBJECTS.find((term) => oldObject[term]);
         if (found && !isNil(newObject[found]) && !isNil(oldObject[found].data.options)) {
-            newObject[found].data.options = oldObject[found].data.options;
+            result = cloneDeep(newObject);
+            const options = oldObject[found].data.options;
+            result[found].data.options = cloneDeep(options);
         }
-        return newObject;
+        return result;
     }
 }
