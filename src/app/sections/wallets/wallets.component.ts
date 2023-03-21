406d49012d2ddd06bacb8d8e179888aa32362eb1
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-03-22 01:12:25 +0400

    add filters

--- src/app/sections/wallets/wallets.component.ts
+++ src/app/sections/wallets/wallets.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
 import { StatWallet } from '@vality/fistful-proto/internal/fistful_stat';
+import { clean } from '@vality/ng-core';
+import { startWith, map } from 'rxjs/operators';
 
+import { WalletParams } from '@cc/app/api/fistful-stat/query-dsl/types/wallet';
+import { QueryParamsService } from '@cc/app/shared/services';
 import { Schema } from '@cc/components/simple-table';
 
 import { FetchWalletsService } from './fetch-wallets.service';
 
+@UntilDestroy()
 @Component({
     selector: 'cc-wallets',
     templateUrl: './wallets.component.html',
@@ -18,7 +24,7 @@ import { FetchWalletsService } from './fetch-wallets.service';
         `,
     ],
 })
-export class WalletsComponent {
+export class WalletsComponent implements OnInit {
     wallets$ = this.fetchWalletsService.searchResult$;
     inProgress$ = this.fetchWalletsService.doAction$;
     hasMore$ = this.fetchWalletsService.hasMore$;
@@ -28,10 +34,30 @@ export class WalletsComponent {
         'identity_id',
         { value: 'created_at', type: 'datetime' },
     ]);
-    control = new FormGroup({});
+    filters = this.fb.group<WalletParams>({
+        party_id: null,
+        identity_id: null,
+        currency_code: null,
+        ...this.qp.params,
+    });
 
-    constructor(private fetchWalletsService: FetchWalletsService) {
-        this.fetchWalletsService.search({});
+    constructor(
+        private fetchWalletsService: FetchWalletsService,
+        private qp: QueryParamsService<WalletParams>,
+        private fb: FormBuilder
+    ) {}
+
+    ngOnInit() {
+        this.filters.valueChanges
+            .pipe(
+                startWith(this.filters.value),
+                map((v) => clean(v)),
+                untilDestroyed(this)
+            )
+            .subscribe((value) => {
+                void this.qp.set(value);
+                this.fetchWalletsService.search(value);
+            });
     }
 
     fetchMore() {
