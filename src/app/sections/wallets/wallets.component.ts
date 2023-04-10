9c317b1b4f6a52687d289211e22e07c5d71c27ea
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-04-10 14:43:03 +0400

    add wallet balance

--- src/app/sections/wallets/wallets.component.ts
+++ src/app/sections/wallets/wallets.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
+import { StatWallet } from '@vality/fistful-proto/internal/fistful_stat';
 import { clean, splitIds } from '@vality/ng-core';
-import { startWith, map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { startWith, map, shareReplay, switchMap, catchError } from 'rxjs/operators';
+import { Memoize } from 'typescript-memoize';
 
+import { AccounterService } from '@cc/app/api/accounter';
 import { WalletParams } from '@cc/app/api/fistful-stat/query-dsl/types/wallet';
+import { ManagementService } from '@cc/app/api/wallet';
 import { QueryParamsService } from '@cc/app/shared/services';
 import {
     createDatetimeFormattedColumn,
@@ -18,25 +23,19 @@ import { FetchWalletsService } from './fetch-wallets.service';
 @Component({
     selector: 'cc-wallets',
     templateUrl: './wallets.component.html',
-    styles: [
-        `
-            :host {
-                display: block;
-                padding: 24px 16px;
-            }
-        `,
-    ],
     providers: [FetchWalletsService],
+    styleUrls: ['wallets.component.scss'],
 })
 export class WalletsComponent implements OnInit {
     wallets$ = this.fetchWalletsService.searchResult$;
     inProgress$ = this.fetchWalletsService.doAction$;
     hasMore$ = this.fetchWalletsService.hasMore$;
-    columns = createGridColumns([
+    columns = createGridColumns<StatWallet>([
         createDescriptionFormattedColumn('name', 'id'),
         'currency_symbolic_code',
         'identity_id',
         createDatetimeFormattedColumn('created_at'),
+        'balance',
     ]);
     filters = this.fb.group<WalletParams>({
         party_id: null,
@@ -46,10 +45,14 @@ export class WalletsComponent implements OnInit {
         ...this.qp.params,
     });
 
+    test$ = this.getBalance('294');
+
     constructor(
         private fetchWalletsService: FetchWalletsService,
         private qp: QueryParamsService<WalletParams>,
-        private fb: FormBuilder
+        private fb: FormBuilder,
+        private walletManagementService: ManagementService,
+        private accounterService: AccounterService
     ) {}
 
     ngOnInit() {
@@ -73,5 +76,19 @@ export class WalletsComponent implements OnInit {
 
     fetchMore() {
         this.fetchWalletsService.fetchMore();
+    }
+
+    @Memoize()
+    getBalance(walletId: string) {
+        return this.walletManagementService.Get(walletId, {}).pipe(
+            switchMap((wallet) => this.accounterService.GetAccountByID(Number(wallet.account.id))),
+            catchError(() =>
+                of({
+                    own_amount: '?',
+                    currency_sym_code: '',
+                })
+            ),
+            shareReplay({ refCount: true, bufferSize: 1 })
+        );
     }
 }
