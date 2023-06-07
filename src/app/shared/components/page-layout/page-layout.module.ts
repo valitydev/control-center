20f8d9b5441a6f910242d11f62436d220d8a845f
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-07 20:54:31 +0400

    new payments layout

--- src/app/shared/components/page-layout/page-layout.module.ts
+++ src/app/shared/components/page-layout/page-layout.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { PageLayoutActionsComponent } from './components/page-layout-actions/page-layout-actions.component';
 import { PageLayoutComponent } from './page-layout.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, FlexModule, MatIconModule, MatProgressSpinnerModule],
     declarations: [PageLayoutComponent, PageLayoutActionsComponent],
     exports: [PageLayoutComponent, PageLayoutActionsComponent],
 })
