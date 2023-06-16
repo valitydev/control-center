2b19d2d39af11202d892a01112b1417c77e21074
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-16 16:04:09 +0400

    support wallet modifications

--- src/app/sections/claim/utils/types/modifications-name-tree.ts
+++ src/app/sections/claim/utils/types/modifications-name-tree.ts
@@ -1,85 +1,8 @@
-import {
-    CashRegisterModificationUnit,
-    ClaimModification,
-    CommentModificationUnit,
-    ContractAdjustmentModificationUnit,
-    ContractModificationUnit,
-    ContractorModificationUnit,
-    DocumentModificationUnit,
-    FileModificationUnit,
-    Modification,
-    PartyModification,
-    PayoutToolModificationUnit,
-    ShopModificationUnit,
-    StatusModificationUnit,
-    WalletModificationUnit,
-    WalletModification,
-    IdentityModification,
-} from '@vality/domain-proto/claim_management';
-import { Overwrite } from 'utility-types';
+import { Modification } from '@vality/domain-proto/claim_management';
 
-type OverwriteAll<T extends object, U extends { [N in keyof T]-?: unknown }> = U;
+type ModificationsNameTree<T> = { [N in keyof T]?: ModificationsNameTree<T[N]> | string };
 
-type ModificationsName<T extends object> = {
-    [P in keyof T]-?: string;
-};
-type ModificationUnitsName<
-    T extends { modification: object },
-    O extends { [N in keyof T['modification']]?: unknown } = Record<never, never>
-> = {
-    modification: Overwrite<ModificationsName<T['modification']>, O>;
-};
-
-type ModificationsNameTree = OverwriteAll<
-    Modification,
-    {
-        claim_modification: OverwriteAll<
-            ClaimModification,
-            {
-                document_modification: ModificationUnitsName<DocumentModificationUnit>;
-                file_modification: ModificationUnitsName<FileModificationUnit>;
-                comment_modification: ModificationUnitsName<CommentModificationUnit>;
-                status_modification: ModificationUnitsName<StatusModificationUnit>;
-                external_info_modification: string;
-            }
-        >;
-        party_modification: OverwriteAll<
-            PartyModification,
-            {
-                contractor_modification: ModificationUnitsName<ContractorModificationUnit>;
-                contract_modification: ModificationUnitsName<
-                    ContractModificationUnit,
-                    {
-                        adjustment_modification: ModificationUnitsName<ContractAdjustmentModificationUnit>;
-                        payout_tool_modification: ModificationUnitsName<PayoutToolModificationUnit>;
-                    }
-                >;
-                shop_modification: ModificationUnitsName<
-                    ShopModificationUnit,
-                    {
-                        cash_register_modification_unit: ModificationUnitsName<CashRegisterModificationUnit>;
-                    }
-                >;
-                wallet_modification: ModificationUnitsName<WalletModificationUnit>;
-            }
-        >;
-        identity_modification: OverwriteAll<
-            IdentityModification,
-            {
-                creation: string;
-            }
-        >;
-        wallet_modification: OverwriteAll<
-            WalletModification,
-            {
-                creation: string;
-                account_creation: string;
-            }
-        >;
-    }
->;
-
-export const MODIFICATIONS_NAME_TREE: ModificationsNameTree = {
+export const MODIFICATIONS_NAME_TREE: ModificationsNameTree<Modification> = {
     claim_modification: {
         document_modification: {
             modification: {
@@ -154,16 +77,19 @@ export const MODIFICATIONS_NAME_TREE: ModificationsNameTree = {
         },
         wallet_modification: {
             modification: {
-                creation: 'Wallet Creation',
-                account_creation: 'Wallet Account Creation',
+                creation: 'Old Wallet Creation',
+                account_creation: 'Old Wallet Account Creation',
             },
         },
     },
     identity_modification: {
-        creation: 'Identity Creation',
+        modification: {
+            creation: 'Identity Creation',
+        },
     },
     wallet_modification: {
-        creation: 'Wallet Creation',
-        account_creation: 'Wallet Account Creation',
+        modification: {
+            creation: 'Wallet Creation',
+        },
     },
 };
