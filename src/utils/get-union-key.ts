308b31a87a8cf6f3b51903f1624b3fcc7bc2b0b3
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-20 17:07:43 +0400

    ref

--- src/utils/get-union-key.ts
+++ src/utils/get-union-key.ts
@@ -4,11 +4,13 @@ import isObject from 'lodash-es/isObject';
 import { ValuesType } from 'utility-types';
 
 export function getUnionKeys<T>(obj: T): (keyof T)[] {
-    return obj ? (Object.keys(obj) as any) : [];
+    return obj ? (Object.keys(obj) as (keyof T)[]) : [];
 }
 
 export function getUnionKeyValue<T>(obj: T): { [K in keyof T]: [K, T[K]] }[keyof T] | null {
-    return isObject(obj) ? (Object.entries(obj).find(([, v]) => !isNil(v)) as any) : null;
+    return isObject(obj)
+        ? (Object.entries(obj).find(([, v]) => !isNil(v)) as { [K in keyof T]: [K, T[K]] }[keyof T])
+        : null;
 }
 
 export function getUnionKey<T>(obj: T): keyof T | null {
--- src/utils/operators/handle-error.ts
+++ src/utils/operators/handle-error.ts
@@ -5,7 +5,7 @@ import { catchError } from 'rxjs/operators';
  * Should be called once
  * It's better to use before the operator "share"
  */
-export function handleError(handle: (error: any) => void) {
+export function handleError(handle: (error: unknown) => void) {
     return <T>(src$: Observable<T>) =>
         src$.pipe(
             catchError((error) => {
