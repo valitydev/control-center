9995b0b98edd11c2bd2e9c6320ce76ecc376ca9f
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-30 15:52:14 +0400

    deprecate

--- src/app/shared/utils/deposit-status.ts
+++ src/app/shared/utils/deposit-status.ts
@@ -2,5 +2,8 @@ import { DepositStatus } from '@vality/fistful-proto/fistful_stat';
 
 import { clearNullFields } from '@cc/utils/thrift-utils';
 
+/**
+ * @deprecated use get union key
+ */
 export const getDepositStatus = (status: DepositStatus): string =>
     Object.keys(clearNullFields(status))[0];
