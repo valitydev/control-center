4bfc2b717769f56f4e1588857f625e1ad0a5e060
A77AY <11846445+A77AY@users.noreply.github.com>
2023-09-26 02:17:14 +0530

    upd create by file table

--- src/app/sections/chargebacks/components/create-chargebacks-by-file-dialog/create-chargebacks-by-file-dialog.component.ts
+++ src/app/sections/chargebacks/components/create-chargebacks-by-file-dialog/create-chargebacks-by-file-dialog.component.ts
@@ -2,23 +2,32 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { InvoicePaymentChargeback } from '@vality/domain-proto/domain';
+import { InvoicePaymentChargebackParams } from '@vality/domain-proto/payment_processing';
 import {
     DialogSuperclass,
     NotifyLogService,
     loadFileContent,
     DEFAULT_DIALOG_CONFIG,
-    Column,
     forkJoinToResult,
+    Column,
 } from '@vality/ng-core';
+import startCase from 'lodash-es/startCase';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { switchMap, map, shareReplay, tap, startWith } from 'rxjs/operators';
 
 import { InvoicingService } from '@cc/app/api/payment-processing';
-import { parseCsv, unifyCsvItems } from '@cc/utils';
+import { parseCsv, unifyCsvItems, getUnionKey } from '@cc/utils';
 
-import { CsvChargeback } from './types/csv-chargeback';
+import { AmountCurrencyService } from '../../../../shared/services';
+
 import { CSV_CHARGEBACK_PROPS } from './types/csv-chargeback-props';
 import { csvChargebacksToInvoicePaymentChargebackParams } from './utils/csv-chargebacks-to-invoice-payment-chargeback-params';
+
+interface ChargebackParams {
+    invoiceId: string;
+    paymentId: string;
+    params: InvoicePaymentChargebackParams;
+}
 
 @UntilDestroy()
 @Component({
@@ -38,9 +47,58 @@ export class CreateChargebacksByFileDialogComponent
     hasHeaderControl = new FormControl(true);
     progress$ = new BehaviorSubject(0);
     upload$ = new BehaviorSubject<File | null>(null);
-    columns: Column<CsvChargeback>[] = CSV_CHARGEBACK_PROPS.map((c) => ({ field: c, header: c }));
+    columns: Column<ChargebackParams>[] = [
+        { field: 'invoiceId', pinned: 'left' },
+        { field: 'paymentId', pinned: 'left' },
+        // { field: 'params.id', pinned: 'left' },
+        {
+            field: 'params.reason',
+            formatter: ({ params }) => startCase(getUnionKey(params.reason.category)),
+            description: ({ params }) => params.reason.code,
+        },
+        {
+            field: 'levy',
+            type: 'currency',
+            formatter: ({ params }) =>
+                this.amountCurrencyService.toMajor(
+                    params.levy.amount,
+                    params.levy.currency.symbolic_code,
+                ),
+            typeParameters: {
+                currencyCode: ({ params }) => params.levy.currency.symbolic_code,
+            },
+        },
+        {
+            field: 'params.body',
+            type: 'currency',
+            formatter: ({ params }) =>
+                this.amountCurrencyService.toMajor(
+                    params.body?.amount,
+                    params.body?.currency?.symbolic_code,
+                ),
+            typeParameters: {
+                currencyCode: ({ params }) => params.body?.currency?.symbolic_code,
+            },
+        },
+        {
+            field: 'params.transaction_info.id',
+            header: 'Transaction id',
+        },
+        {
+            field: 'params.transaction_info.timestamp',
+            header: 'Transaction timestamp',
+            type: 'datetime',
+        },
+        {
+            field: 'params.external_id',
+        },
+        {
+            field: 'params.occurred_at',
+            type: 'datetime',
+        },
+    ];
     defaultFormat = CSV_CHARGEBACK_PROPS.join(';');
-    selectedChargebacks: CsvChargeback[] = [];
+    selectedChargebacks: ChargebackParams[] = [];
     chargebacks$ = combineLatest([
         this.upload$,
         this.hasHeaderControl.valueChanges.pipe(startWith(null)),
@@ -62,6 +120,13 @@ export class CreateChargebacksByFileDialogComponent
             );
             return [];
         }),
+        map((chargebacks) =>
+            chargebacks.map((c) => ({
+                invoiceId: c.invoice_id as string,
+                paymentId: c.payment_id as string,
+                params: csvChargebacksToInvoicePaymentChargebackParams(c),
+            })),
+        ),
         shareReplay({ refCount: true, bufferSize: 1 }),
     );
     successfullyChargebacks: InvoicePaymentChargeback[] = [];
@@ -70,6 +135,7 @@ export class CreateChargebacksByFileDialogComponent
         injector: Injector,
         private invoicingService: InvoicingService,
         private log: NotifyLogService,
+        private amountCurrencyService: AmountCurrencyService,
     ) {
         super(injector);
     }
@@ -84,11 +150,7 @@ export class CreateChargebacksByFileDialogComponent
         const selected = this.selectedChargebacks;
         forkJoinToResult(
             selected.map((c) =>
-                this.invoicingService.CreateChargeback(
-                    c.invoice_id,
-                    c.payment_id,
-                    csvChargebacksToInvoicePaymentChargebackParams(c),
-                ),
+                this.invoicingService.CreateChargeback(c.invoiceId, c.paymentId, c.params),
             ),
             2,
             this.progress$,
