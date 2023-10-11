013afca6494b5f22c165d1de0ac91e8795ffa179
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-10-11 23:56:46 +0700

    new search parties

--- src/app/sections/search-parties/search-parties.component.ts
+++ src/app/sections/search-parties/search-parties.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Party } from '@vality/deanonimus-proto/deanonimus';
+import { Column, createOperationColumn } from '@vality/ng-core';
+import startCase from 'lodash-es/startCase';
 
 import { FetchPartiesService } from '@cc/app/shared/services/fetch-parties.service';
 
+import { getUnionKey } from '../../../utils';
+
 import { PartiesSearchFiltersParams } from './parties-search-filters';
-import { PartyActions } from './parties-table/party-actions';
-import { PartyMenuItemEvent } from './parties-table/party-menu-item-event';
 import { SearchPartiesService } from './search-parties.service';
 
 @Component({
@@ -17,6 +20,50 @@ export class SearchPartiesComponent {
     initSearchParams$ = this.partiesService.data$;
     inProgress$ = this.fetchPartiesService.inProgress$;
     parties$ = this.fetchPartiesService.parties$;
+    columns: Column<Party>[] = [
+        {
+            field: 'email',
+            description: 'id',
+            pinned: 'left',
+            link: (party) => `/party/${party.id}`,
+        },
+        {
+            field: 'blocking',
+            type: 'tag',
+            formatter: (party) => getUnionKey(party.blocking),
+            typeParameters: {
+                label: (party) => startCase(getUnionKey(party.blocking)),
+                tags: {
+                    blocked: { color: 'warn' },
+                    unblocked: { color: 'success' },
+                },
+            },
+        },
+        {
+            field: 'suspension',
+            type: 'tag',
+            formatter: (party) => getUnionKey(party.suspension),
+            typeParameters: {
+                label: (party) => startCase(getUnionKey(party.suspension)),
+                tags: {
+                    suspended: { color: 'warn' },
+                    active: { color: 'success' },
+                },
+            },
+        },
+        {
+            field: 'shops',
+            formatter: (party) => party.shops.size,
+        },
+        createOperationColumn([
+            {
+                label: 'Details',
+                click: (party) => {
+                    this.router.navigate([`/party/${party.id}`]);
+                },
+            },
+        ]),
+    ];
 
     constructor(
         private partiesService: SearchPartiesService,
@@ -27,12 +74,5 @@ export class SearchPartiesComponent {
     searchParamsUpdated($event: PartiesSearchFiltersParams) {
         this.partiesService.preserve($event);
         this.fetchPartiesService.searchParties($event.text);
-    }
-
-    partyMenuItemSelected(event: PartyMenuItemEvent) {
-        switch (event.action) {
-            case PartyActions.NavigateToParty:
-                void this.router.navigate([`/party/${event.partyID}`]);
-        }
     }
 }
