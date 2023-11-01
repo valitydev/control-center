ab1c104cf2b72b6aef70f6825ce5d3b7d36b1ca2
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-11-01 19:21:04 +0700

    fix party shops

--- src/app/sections/party-shops/party-shops.component.ts
+++ src/app/sections/party-shops/party-shops.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { withLatestFrom } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 import { PartyShopsService } from './party-shops.service';
 
@@ -8,7 +10,11 @@ import { PartyShopsService } from './party-shops.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PartyShopsComponent {
-    shopsParty$ = this.partyShopsService.shops$;
+    shopsParty$ = this.partyShopsService.shops$.pipe(
+        withLatestFrom(this.partyShopsService.party$),
+        map(([shops, party]) => shops.map((shop) => ({ shop, party }))),
+        shareReplay({ refCount: true, bufferSize: 1 }),
+    );
     progress$ = this.partyShopsService.progress$;
 
     constructor(private partyShopsService: PartyShopsService) {}
