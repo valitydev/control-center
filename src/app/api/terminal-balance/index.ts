d256ecf14712a4917748e5f2d08287a290a47558
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-04-03 15:51:37 +0900

    init @vality/scrooge-proto

---
+++ src/app/api/terminal-balance/index.ts
@@ -0,0 +1,1 @@
+export * from './terminal-balance.service';
---
+++ src/app/api/terminal-balance/terminal-balance.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {
+    terminal_balance_TerminalServiceCodegenClient,
+    ThriftAstMetadata,
+    terminal_balance_TerminalService,
+} from '@vality/scrooge-proto';
+import { combineLatest, from, map, Observable, switchMap } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { ConfigService } from '../../core/config.service';
+import { KeycloakTokenInfoService, toWachterHeaders } from '../../shared/services';
+
+@Injectable({ providedIn: 'root' })
+export class TerminalBalanceService {
+    private client$: Observable<terminal_balance_TerminalServiceCodegenClient>;
+
+    constructor(
+        private keycloakTokenInfoService: KeycloakTokenInfoService,
+        configService: ConfigService,
+    ) {
+        const headers$ = this.keycloakTokenInfoService.info$.pipe(
+            map(toWachterHeaders('IdentityManagement')),
+        );
+        const metadata$ = from(
+            import('@vality/fistful-proto/metadata.json').then(
+                (m) => m.default as ThriftAstMetadata[],
+            ),
+        );
+        this.client$ = combineLatest([metadata$, headers$]).pipe(
+            switchMap(([metadata, headers]) =>
+                terminal_balance_TerminalService({
+                    metadata,
+                    headers,
+                    logging: environment.logging.requests,
+                    ...configService.config.api.wachter,
+                }),
+            ),
+        );
+    }
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    GetTerminalBalances() {
+        return this.client$.pipe(switchMap((c) => c.GetTerminalBalances()));
+    }
+}
