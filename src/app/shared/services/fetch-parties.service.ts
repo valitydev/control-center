5151a0261f1ac41700bb83cdf891eee38545e264
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-04-25 18:01:03 +0900

    upd FetchPartiesService

--- src/app/shared/services/fetch-parties.service.ts
+++ src/app/shared/services/fetch-parties.service.ts
@@ -1,40 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Party } from '@vality/deanonimus-proto/deanonimus';
-import { NotifyLogService, handleError, progressTo, inProgressFrom } from '@vality/ng-core';
-import { Observable, of, Subject, defer, BehaviorSubject } from 'rxjs';
-import { map, shareReplay, switchMap, debounceTime } from 'rxjs/operators';
+import {
+    NotifyLogService,
+    handleError,
+    FetchOptions,
+    SingleFetchSuperclass,
+} from '@vality/ng-core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { DeanonimusService } from '@cc/app/api/deanonimus';
 
 @Injectable()
-export class FetchPartiesService {
-    parties$: Observable<Party[]> = defer(() => this.searchParties$).pipe(
-        debounceTime(200),
-        switchMap((text) =>
-            text
-                ? this.deanonimusService.searchParty(text).pipe(
-                      map((hits) => hits.map((hit) => hit.party)),
-                      handleError(this.log.error, []),
-                      progressTo(this.progress$),
-                  )
-                : of([]),
-        ),
-        shareReplay(1),
-    );
-    inProgress$ = inProgressFrom(
-        () => this.progress$,
-        () => this.parties$,
-    );
-
-    private progress$ = new BehaviorSubject(0);
-    private searchParties$: Subject<string> = new Subject();
-
+export class FetchPartiesService extends SingleFetchSuperclass<Party, string> {
     constructor(
         private deanonimusService: DeanonimusService,
         private log: NotifyLogService,
-    ) {}
+    ) {
+        super();
+    }
 
-    searchParties(text: string) {
-        this.searchParties$.next(text);
+    protected fetch(text: string, _options: FetchOptions) {
+        return text
+            ? this.deanonimusService.searchParty(text).pipe(
+                  map((hits) => ({ result: hits.map((hit) => hit.party) })),
+                  handleError(this.log.error, { result: [] }),
+              )
+            : of({ result: [] });
     }
 }
