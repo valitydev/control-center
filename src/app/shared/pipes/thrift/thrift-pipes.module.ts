f48249feb58b37359d612c4e5280365d54b10670
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-05-07 23:37:48 +0500

    rem int64 pipe

--- src/app/shared/pipes/thrift/thrift-pipes.module.ts
+++ src/app/shared/pipes/thrift/thrift-pipes.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { KeyTitlePipe } from './key-title.pipe';
 import { MapUnionPipe } from './map-union.pipe';
-import { ThriftInt64Pipe } from './thrift-int64.pipe';
 import { UnionKeyPipe } from './union-key.pipe';
 import { UnionValuePipe } from './union-value.pipe';
 
-const PIPES = [MapUnionPipe, ThriftInt64Pipe, UnionKeyPipe, KeyTitlePipe, UnionValuePipe];
+const PIPES = [MapUnionPipe, UnionKeyPipe, KeyTitlePipe, UnionValuePipe];
 
 @NgModule({
     declarations: PIPES,
