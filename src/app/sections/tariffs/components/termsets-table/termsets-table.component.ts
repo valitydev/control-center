e4b663c8fce9f05af7d66c424b449a5fbe9c3b46
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-05-22 17:08:27 +0500

    show cashFlowsData payments fees

--- src/app/sections/tariffs/components/termsets-table/termsets-table.component.ts
+++ src/app/sections/tariffs/components/termsets-table/termsets-table.component.ts
@@ -1,8 +1,57 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { Rational } from '@vality/domain-proto/internal/base';
+import {
+    CashFlowDecision,
+    CashFlowPosting,
+    CashFlowSelector,
+    CashVolume,
+} from '@vality/domain-proto/internal/domain';
 import { TimedTermSet } from '@vality/dominator-proto/internal/proto/domain';
-import { Column, TableModule } from '@vality/ng-core';
+import { Column, formatCurrency, TableModule } from '@vality/ng-core';
+import { getUnionKey, getUnionValue } from '@vality/ng-thrift';
+import round from 'lodash-es/round';
 
 import { formatPredicate } from '@cc/app/shared/utils/table/format-predicate';
+
+function formatRational(value: Rational) {
+    return `${round((value.p / value.q) * 100, 4)}%`;
+}
+
+function formatCashVolume(d: CashVolume) {
+    switch (getUnionKey(d)) {
+        case 'fixed':
+            return formatCurrency(d?.fixed?.cash?.amount, d?.fixed?.cash?.currency?.symbolic_code);
+        case 'share':
+            return formatRational(d?.share?.parts);
+        case 'product':
+            return `${getUnionKey(d.product).slice(0, -3)}(${Array.from(getUnionValue(d.product))
+                .map((c) => formatCashVolume(c))
+                .join(', ')})`;
+    }
+}
+
+function formatCashFlowPosting(p: CashFlowPosting) {
+    return `${getUnionKey(p?.source)} ${getUnionValue(p?.source)} -> ${getUnionKey(
+        p?.destination,
+    )} ${getUnionValue(p?.destination)}: ${formatCashVolume(p.volume)}`;
+}
+
+function formatCashFlowDecisions(d: CashFlowDecision[]) {
+    if (!d?.length) {
+        return '';
+    }
+    return (
+        '' +
+        d
+            .map(
+                (d) =>
+                    `if ${formatPredicate(d?.if_)} then ${d?.then_?.value
+                        ?.map((p) => formatCashFlowPosting(p))
+                        ?.join(', ')} ${formatCashFlowDecisions(d?.then_?.decisions)}`,
+            )
+            ?.join?.('; ')
+    );
+}
 
 @Component({
     selector: 'cc-termsets-table',
@@ -13,27 +62,29 @@ import { formatPredicate } from '@cc/app/shared/utils/table/format-predicate';
 })
 export class TermsetsTableComponent {
     data = input<TimedTermSet[]>();
-    columns: Column<TimedTermSet>[] = [
+    cashFlowsData = computed(
+        () =>
+            this.data()
+                ?.map?.((t) => t?.terms?.payments?.fees)
+                ?.filter(Boolean),
+    );
+    columns: Column<CashFlowSelector>[] = [
         {
-            field: 'condition',
-            formatter: (d) => formatPredicate(d.terms?.payments?.currencies?.decisions?.[0]?.if_),
+            field: 'decisions',
+            formatter: (d) => formatCashFlowDecisions(d?.decisions),
         },
         {
             field: 'value',
             formatter: (d) =>
-                Array.from(d.terms?.payments?.currencies?.decisions?.[0]?.then_?.value || [])
-                    ?.map?.((v) => v?.symbolic_code)
-                    .join(', '),
-        },
-        {
-            field: 'lower_bound_time',
-            type: 'datetime',
-            formatter: (d) => d.action_time?.lower_bound?.bound_time,
-        },
-        {
-            field: 'upper_bound_time',
-            type: 'datetime',
-            formatter: (d) => d.action_time?.upper_bound?.bound_time,
+                d?.value
+                    ?.filter(
+                        (c) =>
+                            getUnionKey(c?.source) === 'merchant' &&
+                            getUnionKey(c?.destination) === 'system',
+                    )
+                    ?.sort()
+                    ?.map((c) => formatCashVolume(c.volume))
+                    .join(' + '),
         },
     ];
 }
