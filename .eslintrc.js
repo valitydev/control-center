f18951428033bf5ba37f0b78be92480124371a01
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-06-21 03:16:52 +0500

    to @vality/eslint-config-ng

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    extends: '@vality/eslint-config',
+    extends: '@vality/eslint-config-ng',
     overrides: [
-        ...require('@vality/eslint-config/configs').angular('cc').overrides,
-        ...require('@vality/eslint-config/configs').importOrder(['@cc/**']).overrides,
+        ...require('@vality/eslint-config-ng/configs').angular('cc').overrides,
+        ...require('@vality/eslint-config-ng/configs').importOrder(['@cc/**']).overrides,
     ],
 };
