181e29b46ee718f9e8b65421af4197b6a1ff262b
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-06-25 18:22:24 +0500

    products.length === 1

--- src/app/shared/utils/table/format-cash-volume.ts
+++ src/app/shared/utils/table/format-cash-volume.ts
@@ -55,56 +55,69 @@ export function getCashVolumeParts(c: CashVolume[]): CashVolumeParts {
                 }
                 let fixedProduct: CashVolume;
                 let shareProduct: CashVolume;
-                if (getUnionKey(products[0]) === 'fixed' && getUnionKey(products[1]) === 'share') {
+                if (
+                    getUnionKey(products[0]) === 'fixed' &&
+                    (products.length === 1 || getUnionKey(products[1]) === 'share')
+                ) {
                     fixedProduct = products[0];
                     shareProduct = products[1];
                 } else if (
-                    getUnionKey(products[1]) === 'fixed' &&
-                    getUnionKey(products[0]) === 'share'
+                    getUnionKey(products[0]) === 'share' &&
+                    (products.length === 1 || getUnionKey(products[1]) === 'fixed')
                 ) {
-                    fixedProduct = products[1];
                     shareProduct = products[0];
+                    fixedProduct = products[1];
                 } else {
                     return def;
                 }
-                switch (getUnionKey(part.product)) {
-                    case 'min_of': {
-                        if (!isNil(res.min)) {
-                            return def;
-                        }
-                        if (isNil(res.fixed)) {
-                            res.fixed = formatCashVolume(fixedProduct);
-                            res.min = formatCashVolume(shareProduct);
-                        } else if (isNil(res.share)) {
-                            res.share = formatCashVolume(shareProduct);
-                            res.min = formatCashVolume(fixedProduct);
-                        } else {
-                            return def;
-                        }
-                        break;
-                    }
-                    case 'max_of': {
-                        if (!isNil(res.max)) {
-                            return def;
-                        }
-                        if (isNil(res.fixed)) {
-                            res.fixed = formatCashVolume(fixedProduct);
-                            res.max = formatCashVolume(shareProduct);
-                        } else if (isNil(res.share)) {
-                            res.share = formatCashVolume(shareProduct);
-                            res.max = formatCashVolume(fixedProduct);
-                        } else {
-                            return def;
-                        }
-                        break;
-                    }
-                    case 'sum_of': {
-                        if (!isNil(res.fixed) || !isNil(res.share)) {
-                            return def;
-                        }
-                        res.fixed = formatCashVolume(fixedProduct);
+                if (products.length === 1) {
+                    if (shareProduct && isNil(res.share)) {
                         res.share = formatCashVolume(shareProduct);
-                        break;
+                    } else if (fixedProduct && isNil(res.fixed)) {
+                        res.fixed = formatCashVolume(fixedProduct);
+                    } else {
+                        return def;
+                    }
+                } else {
+                    switch (getUnionKey(part.product)) {
+                        case 'min_of': {
+                            if (!isNil(res.min)) {
+                                return def;
+                            }
+                            if (isNil(res.fixed)) {
+                                res.fixed = formatCashVolume(fixedProduct);
+                                res.min = formatCashVolume(shareProduct);
+                            } else if (isNil(res.share)) {
+                                res.share = formatCashVolume(shareProduct);
+                                res.min = formatCashVolume(fixedProduct);
+                            } else {
+                                return def;
+                            }
+                            break;
+                        }
+                        case 'max_of': {
+                            if (!isNil(res.max)) {
+                                return def;
+                            }
+                            if (isNil(res.fixed)) {
+                                res.fixed = formatCashVolume(fixedProduct);
+                                res.max = formatCashVolume(shareProduct);
+                            } else if (isNil(res.share)) {
+                                res.share = formatCashVolume(shareProduct);
+                                res.max = formatCashVolume(fixedProduct);
+                            } else {
+                                return def;
+                            }
+                            break;
+                        }
+                        case 'sum_of': {
+                            if (!isNil(res.fixed) || !isNil(res.share)) {
+                                return def;
+                            }
+                            res.fixed = formatCashVolume(fixedProduct);
+                            res.share = formatCashVolume(shareProduct);
+                            break;
+                        }
                     }
                 }
             }
