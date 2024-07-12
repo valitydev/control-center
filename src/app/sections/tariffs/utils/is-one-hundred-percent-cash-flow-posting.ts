b83a666bd76cb6bb1388a79ef2631b05c7775485
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-07-12 17:30:13 +0500

    fix

--- src/app/sections/tariffs/utils/is-one-hundred-percent-cash-flow-posting.ts
+++ src/app/sections/tariffs/utils/is-one-hundred-percent-cash-flow-posting.ts
@@ -1,5 +1,5 @@
 import { CashFlowPosting } from '@vality/domain-proto/domain';
 
 export function isOneHundredPercentCashFlowPosting(v: CashFlowPosting) {
-    return !(v?.volume?.share?.parts?.p === 1 && v?.volume?.share?.parts?.q === 1);
+    return v?.volume?.share?.parts?.p === 1 && v?.volume?.share?.parts?.q === 1;
 }
