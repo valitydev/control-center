ffd7da2fda04b970cabb059d8ffb7bc416890ce6
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-10-12 02:34:56 +0700

    fix chargebacks cols

--- src/app/shared/components/chargebacks-table/chargebacks-table.component.ts
+++ src/app/shared/components/chargebacks-table/chargebacks-table.component.ts
@@ -6,13 +6,10 @@ import {
     EventEmitter,
     booleanAttribute,
     input,
-    Injector,
-    computed,
-    runInInjectionContext,
     DestroyRef,
     model,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { StatChargeback } from '@vality/magista-proto/magista';
 import {
@@ -51,93 +48,86 @@ export class ChargebacksTableComponent {
     @Output() update = new EventEmitter<LoadOptions>();
     @Output() more = new EventEmitter<void>();
 
-    columns = computed<Column2<StatChargeback>[]>(() =>
-        runInInjectionContext(this.injector, () => [
-            { field: 'chargeback_id', header: 'Id' },
-            {
-                field: 'chargeback_reason',
-                header: 'Reason',
-                cell: (d) => ({
-                    value: startCase(getUnionKey(d.chargeback_reason.category)),
-                    description: d.chargeback_reason.code,
-                }),
-            },
-            {
-                field: 'chargeback_status',
-                header: 'Status',
-                cell: (d) => ({
-                    value: startCase(getUnionKey(d.chargeback_status)),
-                    color: (
-                        {
-                            pending: 'pending',
-                            accepted: 'success',
-                            rejected: 'warn',
-                            cancelled: 'neutral',
-                        } as const
-                    )[getUnionKey(d.chargeback_status)],
-                }),
-            },
-            createCurrencyColumn(
-                (d) => ({ amount: d.amount, code: d.currency_code.symbolic_code }),
-                {
-                    header: 'Amount',
-                },
-            ),
-            createCurrencyColumn(
-                (d) => ({ amount: d.levy_amount, code: d.levy_currency_code.symbolic_code }),
-                { header: 'Levy Amount' },
-            ),
-            createCurrencyColumn((d) => ({ amount: d.fee, code: d.currency_code.symbolic_code }), {
-                header: 'Fee',
+    columns: Column2<StatChargeback>[] = [
+        { field: 'chargeback_id', header: 'Id' },
+        {
+            field: 'chargeback_reason',
+            header: 'Reason',
+            cell: (d) => ({
+                value: startCase(getUnionKey(d.chargeback_reason.category)),
+                description: d.chargeback_reason.code,
             }),
-            createCurrencyColumn(
-                (d) => ({ amount: d.provider_fee, code: d.currency_code.symbolic_code }),
-                { header: 'Provider Fee' },
-            ),
-            createCurrencyColumn(
-                (d) => ({ amount: d.external_fee, code: d.currency_code.symbolic_code }),
-                { header: 'External Fee' },
-            ),
-            {
-                field: 'stage',
-                cell: (d) => ({ value: startCase(getUnionKey(d.stage)) }),
-            },
-            {
-                field: 'invoice_id',
-                header: {
-                    value: 'Invoice Id',
-                    description: 'Payment Id',
-                },
-                cell: (d) => ({
-                    description: d.payment_id,
-                    link: () =>
-                        `/party/${d.party_id}/invoice/${d.invoice_id}/payment/${d.payment_id}`,
-                }),
-                hidden: this.onePayment(),
-            },
-            {
-                field: 'created_at',
-                cell: { type: 'datetime' },
-            },
-            createPartyColumn((d) => ({ id: d.party_id }), { hidden: this.onePayment() }),
-            createShopColumn((d) => ({ shopId: d.shop_id, partyId: d.party_id }), {
-                hidden: this.onePayment(),
-            }),
-            createMenuColumn((d) => ({
-                items: [
+        },
+        {
+            field: 'chargeback_status',
+            header: 'Status',
+            cell: (d) => ({
+                value: startCase(getUnionKey(d.chargeback_status)),
+                color: (
                     {
-                        label: 'Change status',
-                        click: () => {
-                            this.changeStatus(d);
-                        },
+                        pending: 'pending',
+                        accepted: 'success',
+                        rejected: 'warn',
+                        cancelled: 'neutral',
+                    } as const
+                )[getUnionKey(d.chargeback_status)],
+            }),
+        },
+        createCurrencyColumn((d) => ({ amount: d.amount, code: d.currency_code.symbolic_code }), {
+            header: 'Amount',
+        }),
+        createCurrencyColumn(
+            (d) => ({ amount: d.levy_amount, code: d.levy_currency_code.symbolic_code }),
+            { header: 'Levy Amount' },
+        ),
+        createCurrencyColumn((d) => ({ amount: d.fee, code: d.currency_code.symbolic_code }), {
+            header: 'Fee',
+        }),
+        createCurrencyColumn(
+            (d) => ({ amount: d.provider_fee, code: d.currency_code.symbolic_code }),
+            { header: 'Provider Fee' },
+        ),
+        createCurrencyColumn(
+            (d) => ({ amount: d.external_fee, code: d.currency_code.symbolic_code }),
+            { header: 'External Fee' },
+        ),
+        {
+            field: 'stage',
+            cell: (d) => ({ value: startCase(getUnionKey(d.stage)) }),
+        },
+        {
+            field: 'invoice_id',
+            header: {
+                value: 'Invoice Id',
+                description: 'Payment Id',
+            },
+            cell: (d) => ({
+                description: d.payment_id,
+                link: () => `/party/${d.party_id}/invoice/${d.invoice_id}/payment/${d.payment_id}`,
+            }),
+            hidden: toObservable(this.onePayment),
+        },
+        {
+            field: 'created_at',
+            cell: { type: 'datetime' },
+        },
+        createPartyColumn((d) => ({ id: d.party_id }), { hidden: toObservable(this.onePayment) }),
+        createShopColumn((d) => ({ shopId: d.shop_id, partyId: d.party_id }), {
+            hidden: toObservable(this.onePayment),
+        }),
+        createMenuColumn((d) => ({
+            items: [
+                {
+                    label: 'Change status',
+                    click: () => {
+                        this.changeStatus(d);
                     },
-                ],
-            })),
-        ]),
-    );
+                },
+            ],
+        })),
+    ];
 
     constructor(
-        private injector: Injector,
         private dialogService: DialogService,
         private dr: DestroyRef,
     ) {}
