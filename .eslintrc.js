d113b021674a84cf62f44f66481253e4cb107af7
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-12-17 04:28:56 +0700

    init new eslint configs

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: '@vality/eslint-config-ng',
-    overrides: [
-        ...require('@vality/eslint-config-ng/configs').angular('cc').overrides,
-        ...require('@vality/eslint-config-ng/configs').importOrder(['@cc/**']).overrides,
-    ],
-};
---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+const nx = require('@nx/eslint-plugin');
+
+module.exports = [
+    ...nx.configs['flat/base'],
+    ...nx.configs['flat/typescript'],
+    ...nx.configs['flat/javascript'],
+    {
+        ignores: ['**/dist'],
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+        rules: {
+            '@nx/enforce-module-boundaries': [
+                'error',
+                {
+                    enforceBuildableLibDependency: true,
+                    allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+                    depConstraints: [
+                        {
+                            sourceTag: '*',
+                            onlyDependOnLibsWithTags: ['*'],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+        // Override or add rules here
+        rules: {
+            '@typescript-eslint/no-inferrable-types': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: true,
+                },
+            ],
+        },
+    },
+];
