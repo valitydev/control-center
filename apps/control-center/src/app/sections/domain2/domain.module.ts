1328875fe4cef7bce03958f2f623c2ae6bbf82cb
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-04-29 18:33:54 +0600

    clean

--- apps/control-center/src/app/sections/domain2/domain.module.ts
+++ apps/control-center/src/app/sections/domain2/domain.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import { DomainRoutingModule } from './domain-routing.module';
-import { MetadataService } from './services/metadata.service';
 
 @NgModule({
     imports: [DomainRoutingModule],
-    providers: [MetadataService],
 })
 export class DomainModule {}
