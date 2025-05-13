ec9ce3f4351f1b7f184ce070dbd5f9c3ef1b36cf
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-05-13 20:50:03 +0500

    add objects

--- apps/control-center/src/app/api/domain-config/stores/domain2-store.service.ts
+++ apps/control-center/src/app/api/domain-config/stores/domain2-store.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { DomainObject, Reference, ReflessDomainObject } from '@vality/domain-proto/domain';
-import { Operation, Version } from '@vality/domain-proto/domain_config_v2';
+import { Operation, Version, VersionedObject } from '@vality/domain-proto/domain_config_v2';
 import { NotifyLogService, switchCombineWith } from '@vality/matez';
-import { getUnionKey } from '@vality/ng-thrift';
-import { EMPTY, catchError, tap } from 'rxjs';
+import { getUnionKey, getUnionValue } from '@vality/ng-thrift';
+import { EMPTY, Observable, catchError, map, share, tap } from 'rxjs';
 
 import { RepositoryClientService } from '../repository-client.service';
 import { Repository2Service } from '../repository2.service';
 import { DomainSecretService } from '../services';
+import { createObjectHash } from '../utils/create-object-hash';
 
 import { AuthorStoreService } from './author-store.service';
 
@@ -17,8 +18,15 @@ import { AuthorStoreService } from './author-store.service';
 })
 export class Domain2StoreService {
     version = rxResource({
-        loader: () => this.repositoryService.GetLatestVersion(),
+        loader: () =>
+            this.repositoryService.GetLatestVersion().pipe(
+                tap((version) => this.setObjectsVersion(version)),
+                share(),
+            ),
     });
+
+    private objectsVersion: number = null;
+    private objects = new Map<string, VersionedObject>();
 
     constructor(
         private repositoryService: Repository2Service,
@@ -28,9 +36,10 @@ export class Domain2StoreService {
         private domainSecretService: DomainSecretService,
     ) {}
 
-    getObject(ref: Reference) {
-        this.getSecretObject(ref).pipe(
+    getObject(ref: Reference): Observable<VersionedObject> {
+        return this.getSecretObject(ref).pipe(
             switchCombineWith((obj) => [this.domainSecretService.reduceObject(obj.object)]),
+            map(([{ info }, object]) => ({ info, object })),
         );
     }
 
@@ -98,7 +107,28 @@ export class Domain2StoreService {
         return this.commit(refs.map((ref) => ({ remove: { ref } })));
     }
 
-    private getSecretObject(ref: Reference) {
-        return this.repositoryClientService.CheckoutObject({}, ref);
+    private getSecretObject(ref: Reference, version = this.version.value()) {
+        return this.repositoryClientService.CheckoutObject({ version }, ref).pipe(
+            tap((obj) => {
+                this.setObject(obj, version);
+            }),
+            share(),
+        );
+    }
+
+    private setObjectsVersion(newVersion: number) {
+        if (this.objectsVersion !== newVersion) {
+            this.objectsVersion = newVersion;
+            this.objects.clear();
+        }
+    }
+
+    private setObject(obj: VersionedObject, version: number) {
+        if (version === this.objectsVersion) {
+            this.objects.set(
+                createObjectHash({ [getUnionKey(obj.object)]: getUnionValue(obj.object).ref }),
+                obj,
+            );
+        }
     }
 }
