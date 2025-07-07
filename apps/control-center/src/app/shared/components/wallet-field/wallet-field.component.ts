2a1c0a4d093db63aa4f2472b39531acbdb929f10
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-07-07 22:30:19 +0500

    upd WalletFieldComponent

--- apps/control-center/src/app/shared/components/wallet-field/wallet-field.component.ts
+++ apps/control-center/src/app/shared/components/wallet-field/wallet-field.component.ts
@@ -1,41 +1,25 @@
-import {
-    AfterViewInit,
-    Component,
-    DestroyRef,
-    Input,
-    booleanAttribute,
-    inject,
-} from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Deanonimus } from '@vality/deanonimus-proto/deanonimus';
-import { WalletID } from '@vality/domain-proto/domain';
+import { Component, Input, booleanAttribute, inject } from '@angular/core';
+import { DomainObjectType, WalletID } from '@vality/domain-proto/domain';
 import {
     FormControlSuperclass,
-    NotifyLogService,
     Option,
     SelectFieldComponent,
     createControlProviders,
-    debounceTimeWithFirst,
-    progressTo,
 } from '@vality/matez';
-import { BehaviorSubject, Observable, ReplaySubject, Subject, concat, forkJoin, of } from 'rxjs';
-import { catchError, distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { DEBOUNCE_TIME_MS } from '../../../tokens';
+import { FetchDomainObjectsService } from '../../../api/domain-config';
 
 @Component({
     selector: 'cc-wallet-field',
     templateUrl: 'wallet-field.component.html',
-    providers: createControlProviders(() => WalletFieldComponent),
+    providers: [...createControlProviders(() => WalletFieldComponent), FetchDomainObjectsService],
     standalone: false,
 })
-export class WalletFieldComponent
-    extends FormControlSuperclass<WalletID | WalletID[]>
-    implements AfterViewInit
-{
-    private deanonimusService = inject(Deanonimus);
-    private log = inject(NotifyLogService);
-    private destroyRef = inject(DestroyRef);
+export class WalletFieldComponent extends FormControlSuperclass<WalletID | WalletID[]> {
+    private fetchDomainObjectsService = inject(FetchDomainObjectsService);
+
     @Input() label: string;
     @Input({ transform: booleanAttribute }) required: boolean;
     @Input() size?: SelectFieldComponent['size'];
@@ -43,60 +27,21 @@ export class WalletFieldComponent
     @Input() hint?: string;
     @Input({ transform: booleanAttribute }) multiple = false;
 
-    options$ = new ReplaySubject<Option<WalletID>[]>(1);
-    searchChange$ = new Subject<string>();
-    progress$ = new BehaviorSubject(0);
+    options$: Observable<Option<WalletID>[]> = this.fetchDomainObjectsService.result$.pipe(
+        map((objs) =>
+            objs.map((obj) => ({
+                value: obj.ref.wallet_config.id,
+                label: obj.name || obj.ref.wallet_config.id,
+                description: obj.description,
+            })),
+        ),
+    );
+    progress$ = this.fetchDomainObjectsService.isLoading$;
 
-    private debounceTimeMs = inject(DEBOUNCE_TIME_MS);
-
-    ngAfterViewInit() {
-        concat(
-            of(this.control.value).pipe(
-                switchMap((term) =>
-                    forkJoin(
-                        (Array.isArray(term) ? term : [term ?? '']).map((t) => this.findOption(t)),
-                    ),
-                ),
-                map((o) => o.filter(Boolean)),
-            ),
-            this.searchChange$.pipe(
-                distinctUntilChanged(),
-                tap(() => {
-                    this.options$.next([]);
-                }),
-                debounceTimeWithFirst(this.debounceTimeMs),
-                switchMap((term) => this.searchOptions(term)),
-            ),
-        )
-            .pipe(takeUntilDestroyed(this.destroyRef))
-            .subscribe((options) => {
-                this.options$.next(options);
-            });
-    }
-
-    private searchOptions(str: string): Observable<Option<WalletID>[]> {
-        if (!str) {
-            return of([]);
-        }
-        return this.deanonimusService.searchWalletText(str).pipe(
-            map((wallets) =>
-                wallets.map((p) => ({
-                    label: p.wallet.name,
-                    value: p.wallet.id,
-                    description: p.wallet.id,
-                })),
-            ),
-            catchError((err) => {
-                this.log.error(err, 'Search error');
-                return of([]);
-            }),
-            progressTo(this.progress$),
-        );
-    }
-
-    private findOption(id: WalletID) {
-        return this.searchOptions(id).pipe(
-            map((options) => (options?.length ? options.find((o) => o.value === id) : null)),
+    search(search: string) {
+        this.fetchDomainObjectsService.load(
+            { type: DomainObjectType.wallet_config, query: search },
+            { size: 1000 },
         );
     }
 }
