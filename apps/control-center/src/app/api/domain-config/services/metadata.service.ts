667f9b25d8b21361b8ba7d819c77279aea868135
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-07-12 19:52:53 +0400

    clean

--- apps/control-center/src/app/api/domain-config/services/metadata.service.ts
+++ apps/control-center/src/app/api/domain-config/services/metadata.service.ts
@@ -1,42 +1,12 @@
 import { Injectable } from '@angular/core';
 import { metadata$ } from '@vality/domain-proto';
-import { Reference } from '@vality/domain-proto/domain';
 import { Field } from '@vality/thrift-ts';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map, withLatestFrom } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class MetadataService {
     private metadata$ = metadata$;
-
-    getDomainObjectType(ref: Reference): Observable<string | null> {
-        if (!ref) {
-            return of(null);
-        }
-        const keys = Object.keys(ref);
-        if (keys.length !== 1) {
-            return of(null);
-        }
-        const searchName = keys[0];
-        return this.getDomainFields().pipe(
-            map((d) => {
-                const found = d.find(({ name }) => name === searchName);
-                return found ? (found.type as string) : null;
-            }),
-        );
-    }
-
-    getDomainFieldByName(fieldName: string): Observable<Field> {
-        return this.getDomainFields().pipe(
-            map((fields) => fields.find((f) => f.name === fieldName)),
-        );
-    }
-
-    getDomainFieldByType(fieldType: string): Observable<Field> {
-        return this.getDomainFields().pipe(
-            map((fields) => fields.find((f) => f.type === fieldType)),
-        );
-    }
 
     getDomainObjectDataFieldByName(fieldName: string): Observable<Field> {
         return this.getDomainFields().pipe(
