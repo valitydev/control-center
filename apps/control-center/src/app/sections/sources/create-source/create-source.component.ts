3333cdacbd7d7da02eb0e947ff45ab79acd951c9
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-07-15 19:19:59 +0400

    rem fistful admin

--- apps/control-center/src/app/sections/sources/create-source/create-source.component.ts
+++ apps/control-center/src/app/sections/sources/create-source/create-source.component.ts
@@ -1,8 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { DialogSuperclass, NotifyLogService } from '@vality/matez';
-
-import { FistfulAdminService } from '../../../api/fistful-admin';
+import { throwError } from 'rxjs';
 
 @Component({
     selector: 'cc-create-source',
@@ -10,12 +9,14 @@ import { FistfulAdminService } from '../../../api/fistful-admin';
     standalone: false,
 })
 export class CreateSourceComponent extends DialogSuperclass<void> {
-    private fistfulAdminService = inject(FistfulAdminService);
+    // private fistfulAdminService = inject(FistfulAdminService);
     private log = inject(NotifyLogService);
     control = new FormControl();
 
     create() {
-        this.fistfulAdminService.CreateSource(this.control.value).subscribe({
+        // TODO: Wait fistful-admin service
+        // this.fistfulAdminService.CreateSource(this.control.value).subscribe({
+        throwError(new Error('No fistful-admin service')).subscribe({
             next: () => {
                 this.closeWithSuccess();
                 this.log.success('Source created successfully!');
