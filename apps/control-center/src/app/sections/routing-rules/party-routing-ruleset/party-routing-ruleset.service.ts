83757d61a183a59cbf84fe9bf93f55640de4a36d
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-07-15 19:35:39 +0400

    upd PartyRoutingRulesetService

--- apps/control-center/src/app/sections/routing-rules/party-routing-ruleset/party-routing-ruleset.service.ts
+++ apps/control-center/src/app/sections/routing-rules/party-routing-ruleset/party-routing-ruleset.service.ts
@@ -2,13 +2,11 @@ import { DestroyRef, Injectable, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { PartyManagement } from '@vality/domain-proto/payment_processing';
-import { FistfulStatistics } from '@vality/fistful-proto/fistful_stat';
 import { NotifyLogService } from '@vality/matez';
 import isNil from 'lodash-es/isNil';
 import { Observable, combineLatest, defer, of } from 'rxjs';
 import { map, shareReplay, switchMap, take, tap } from 'rxjs/operators';
 
-import { createDsl } from '../../../api/fistful-stat';
 import { PartyDelegateRulesetsService } from '../party-delegate-rulesets';
 import { RoutingRulesService } from '../services/routing-rules';
 
@@ -19,14 +17,13 @@ export class PartyRoutingRulesetService {
     private route = inject(ActivatedRoute);
     private partyManagementService = inject(PartyManagement);
     private routingRulesService = inject(RoutingRulesService);
-    private fistfulStatistics = inject(FistfulStatistics);
-    private destroyRef = inject(DestroyRef);
+    private dr = inject(DestroyRef);
     private partyDelegateRulesetsService = inject(PartyDelegateRulesetsService);
     private log = inject(NotifyLogService);
 
     partyID$ = this.route.params.pipe(
         map((r) => r['partyID']),
-        takeUntilDestroyed(this.destroyRef),
+        takeUntilDestroyed(this.dr),
         shareReplay(1),
     ) as Observable<string>;
     refID$ = this.route.params.pipe(
@@ -44,35 +41,28 @@ export class PartyRoutingRulesetService {
                 this.log.error('Unknown delegate');
             }
         }),
-        takeUntilDestroyed(this.destroyRef),
+        takeUntilDestroyed(this.dr),
         shareReplay(1),
     );
 
     shops$ = defer(() => this.party$).pipe(
-        map((p) => p.shops),
-        map((shops) => Array.from(shops.values())),
+        map((p) => Array.from(p.shops.values())),
         shareReplay({ refCount: true, bufferSize: 1 }),
     );
-    wallets$ = defer(() => this.partyID$).pipe(
-        switchMap((partyID) =>
-            this.fistfulStatistics.GetWallets({
-                dsl: createDsl({ wallets: { party_id: partyID } }),
-            }),
-        ),
-        map((v) => v?.data?.wallets),
-        takeUntilDestroyed(this.destroyRef),
-        shareReplay(1),
+    wallets$ = defer(() => this.party$).pipe(
+        map((p) => Array.from(p.wallets.values())),
+        shareReplay({ refCount: true, bufferSize: 1 }),
     );
 
     partyRuleset$ = combineLatest([this.routingRulesService.rulesets$, this.refID$]).pipe(
         map(([rules, refID]) => rules.find((r) => r?.ref?.id === refID)),
-        takeUntilDestroyed(this.destroyRef),
+        takeUntilDestroyed(this.dr),
         shareReplay(1),
     );
 
     private party$ = this.partyID$.pipe(
         switchMap((partyID) => this.partyManagementService.Get(partyID)),
-        takeUntilDestroyed(this.destroyRef),
+        takeUntilDestroyed(this.dr),
         shareReplay(1),
     );
 
