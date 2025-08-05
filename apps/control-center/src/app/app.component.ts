df358a44dd238c307c2d25f6783566d55bfa374f
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-08-05 23:10:36 +0400

    clean

--- apps/control-center/src/app/app.component.ts
+++ apps/control-center/src/app/app.component.ts
@@ -171,7 +171,6 @@ const createNavLinks = (): Link[] => [
 @Component({
     selector: 'cc-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
     standalone: false,
 })
 export class AppComponent {
