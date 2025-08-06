01f689a4a6fa8b10282b8e2411f9c3d793ec4230
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-08-06 23:01:39 +0400

    init matez

---
+++ projects/matez/src/lib/matez.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-matez',
+  imports: [],
+  template: `
+    <p>
+      matez works!
+    </p>
+  `,
+  styles: ``
+})
+export class Matez {
+
+}
---
+++ projects/matez/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of matez
+ */
+
+export * from './lib/matez';
