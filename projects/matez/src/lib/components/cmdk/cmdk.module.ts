91c6f569d9668db2413a82b0abc22051f7cc8c38
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-08-11 16:42:36 +0400

    fix

--- projects/matez/src/lib/components/cmdk/cmdk.module.ts
+++ projects/matez/src/lib/components/cmdk/cmdk.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { CmdkService } from './cmdk.service';
 import { CmdkButtonComponent } from './components/cmdk-button.component';
 
 @NgModule({
     imports: [CmdkButtonComponent],
-    providers: [CmdkService],
     exports: [CmdkButtonComponent],
 })
 export class CmdkModule {}
