34d03b6427b74b6b411691422c0f3f240bab60a2
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-08-18 20:45:16 +0400

    add studio services

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,65 +1,131 @@
-import { createThriftServices } from '~/utils';
+import { Observable } from 'rxjs';
 
-const SERVICES = [
+import { metadata$ as domainMetadata$ } from '@vality/domain-proto';
+import { metadata$ as dominatorMetadata$ } from '@vality/dominator-proto';
+import { metadata$ as fistfulMetadata$ } from '@vality/fistful-proto';
+import { metadata$ as machinegunMetadata$ } from '@vality/machinegun-proto';
+import { metadata$ as magistaMetadata$ } from '@vality/magista-proto';
+import { ThriftAstMetadata } from '@vality/ng-thrift';
+import { metadata$ as repairerMetadata$ } from '@vality/repairer-proto';
+import { metadata$ as scroogeMetadata$ } from '@vality/scrooge-proto';
+
+import { ThriftService, createThriftServices } from '~/utils';
+
+export interface MetadataThriftService extends ThriftService {
+    metadata$: Observable<ThriftAstMetadata[]>;
+    namespace: string;
+    service: string;
+}
+
+export const services = [
+    // Domain
     {
         name: 'DMT',
         loader: () => import('@vality/domain-proto/domain_config_v2').then((m) => m.Repository),
+        metadata$: domainMetadata$,
+        namespace: 'domain_config_v2',
+        service: 'Repository',
     },
     {
         name: 'DMTClient',
         loader: () =>
             import('@vality/domain-proto/domain_config_v2').then((m) => m.RepositoryClient),
+        metadata$: domainMetadata$,
+        namespace: 'domain_config_v2',
+        service: 'RepositoryClient',
     },
     {
         name: 'DMTAuthor',
         loader: () =>
             import('@vality/domain-proto/domain_config_v2').then((m) => m.AuthorManagement),
-    },
-    {
-        name: 'RepairManagement',
-        loader: () => import('@vality/repairer-proto/repairer').then((m) => m.RepairManagement),
-    },
-    {
-        name: 'Scrooge',
-        loader: () => import('@vality/scrooge-proto/account_balance').then((m) => m.AccountService),
-    },
-    {
-        name: 'MerchantStatistics',
-        loader: () =>
-            import('@vality/magista-proto/magista').then((m) => m.MerchantStatisticsService),
-    },
-    {
-        name: 'Dominator',
-        loader: () => import('@vality/dominator-proto/dominator').then((m) => m.DominatorService),
-    },
-    {
-        name: 'Automaton',
-        loader: () => import('@vality/machinegun-proto/state_processing').then((m) => m.Automaton),
+        metadata$: domainMetadata$,
+        namespace: 'domain_config_v2',
+        service: 'AuthorManagement',
     },
     {
         name: 'Invoicing',
         loader: () => import('@vality/domain-proto/payment_processing').then((m) => m.Invoicing),
+        metadata$: domainMetadata$,
+        namespace: 'payment_processing',
+        service: 'Invoicing',
     },
     {
         name: 'PartyManagement',
         loader: () =>
             import('@vality/domain-proto/payment_processing').then((m) => m.PartyManagement),
+        metadata$: domainMetadata$,
+        namespace: 'payment_processing',
+        service: 'PartyManagement',
     },
+    // Repairer
+    {
+        name: 'RepairManagement',
+        loader: () => import('@vality/repairer-proto/repairer').then((m) => m.RepairManagement),
+        metadata$: repairerMetadata$,
+        namespace: 'repairer',
+        service: 'RepairManagement',
+    },
+    // Scrooge
+    {
+        name: 'Scrooge',
+        loader: () => import('@vality/scrooge-proto/account_balance').then((m) => m.AccountService),
+        metadata$: scroogeMetadata$,
+        namespace: 'account_balance',
+        service: 'AccountService',
+    },
+    // Magista
+    {
+        name: 'MerchantStatistics',
+        loader: () =>
+            import('@vality/magista-proto/magista').then((m) => m.MerchantStatisticsService),
+        metadata$: magistaMetadata$,
+        namespace: 'magista',
+        service: 'MerchantStatisticsService',
+    },
+    // Dominator
+    {
+        name: 'Dominator',
+        loader: () => import('@vality/dominator-proto/dominator').then((m) => m.DominatorService),
+        metadata$: dominatorMetadata$,
+        namespace: 'dominator',
+        service: 'DominatorService',
+    },
+    // Machinegun
+    {
+        name: 'Automaton',
+        loader: () => import('@vality/machinegun-proto/state_processing').then((m) => m.Automaton),
+        metadata$: machinegunMetadata$,
+        namespace: 'state_processing',
+        service: 'Automaton',
+    },
+    // Fistful
     {
         name: 'DepositManagement',
         loader: () => import('@vality/fistful-proto/deposit').then((m) => m.Management),
+        metadata$: fistfulMetadata$,
+        namespace: 'deposit',
+        service: 'Management',
     },
     {
         name: 'FistfulStatistics',
         loader: () => import('@vality/fistful-proto/fistful_stat').then((m) => m.FistfulStatistics),
+        metadata$: fistfulMetadata$,
+        namespace: 'fistful_stat',
+        service: 'FistfulStatistics',
     },
     {
         name: 'WithdrawalManagement',
         loader: () => import('@vality/fistful-proto/withdrawal').then((m) => m.Management),
+        metadata$: fistfulMetadata$,
+        namespace: 'withdrawal',
+        service: 'Management',
     },
     {
         name: 'SourceManagement',
         loader: () => import('@vality/fistful-proto/source').then((m) => m.Management),
+        metadata$: fistfulMetadata$,
+        namespace: 'source',
+        service: 'Management',
     },
 ] as const;
 
@@ -81,4 +147,4 @@ export const {
         SourceManagement: ThriftSourceManagementService,
     },
     provideThriftServices,
-} = createThriftServices(SERVICES);
+} = createThriftServices(services);
--- src/app/studio/studio.component.ts
+++ src/app/studio/studio.component.ts
@@ -1,12 +1,93 @@
-import { Component } from '@angular/core';
+import { Observable, combineLatest, map, shareReplay, skipWhile } from 'rxjs';
 
-import { SelectFieldModule } from '@vality/matez';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 
+import {
+    Option,
+    SelectFieldModule,
+    compareDifferentTypes,
+    getValueChanges,
+    switchCombineWith,
+} from '@vality/matez';
+import { ThriftEditorModule, ValueTypeTitlePipe } from '@vality/ng-thrift';
+import { Method } from '@vality/thrift-ts';
+
+import { MetadataThriftService, services } from '~/api/services';
 import { PageLayoutModule } from '~/components/page-layout';
 
 @Component({
     selector: 'cc-studio',
-    imports: [PageLayoutModule, SelectFieldModule],
+    imports: [
+        PageLayoutModule,
+        SelectFieldModule,
+        ThriftEditorModule,
+        ReactiveFormsModule,
+        CommonModule,
+        ValueTypeTitlePipe,
+    ],
     templateUrl: './studio.component.html',
 })
-export class StudioComponent {}
+export class StudioComponent implements OnInit {
+    private fb = inject(NonNullableFormBuilder);
+    private dr = inject(DestroyRef);
+
+    studioGroup = this.fb.group<{
+        service: MetadataThriftService | null;
+        method: Method | null;
+        params: object | null;
+    }>({
+        service: null,
+        method: null,
+        params: null,
+    });
+
+    serviceOptions: Option<MetadataThriftService>[] = services
+        .map((s) => ({
+            label: s.service,
+            value: s,
+        }))
+        .sort((a, b) => compareDifferentTypes(a.label, b.label));
+    namespaceMetadata$ = getValueChanges(this.studioGroup.controls.service).pipe(
+        skipWhile((service) => !service),
+        switchCombineWith((service) => [service?.metadata$]),
+        map(([service, metadata]) => metadata.find((m) => m.name === service.namespace)),
+        shareReplay({ refCount: true, bufferSize: 1 }),
+    );
+    serviceMetadata$ = combineLatest([
+        getValueChanges(this.studioGroup.controls.service),
+        this.namespaceMetadata$,
+    ]).pipe(
+        map(([service, metadata]) => metadata.ast.service[service.service]),
+        shareReplay({ refCount: true, bufferSize: 1 }),
+    );
+    methodOptions$: Observable<Option<Method>[]> = this.serviceMetadata$.pipe(
+        map((metadata) =>
+            Object.entries(metadata.functions)
+                .map(([name, method]) => ({
+                    label: name,
+                    value: method,
+                }))
+                .sort((a, b) => compareDifferentTypes(a.label, b.label)),
+        ),
+        shareReplay({ refCount: true, bufferSize: 1 }),
+    );
+
+    ngOnInit() {
+        this.studioGroup
+            .get('service')
+            .valueChanges.pipe(takeUntilDestroyed(this.dr))
+            .subscribe(() => {
+                this.studioGroup.get('method').reset();
+                this.studioGroup.get('params').reset();
+            });
+        this.studioGroup
+            .get('method')
+            .valueChanges.pipe(takeUntilDestroyed(this.dr))
+            .subscribe(() => {
+                this.studioGroup.get('params').reset();
+            });
+    }
+}
