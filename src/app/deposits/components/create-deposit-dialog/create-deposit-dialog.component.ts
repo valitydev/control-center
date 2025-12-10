bc8b7ab6b13fd864a2db7aa1ab353afa3b2c48d3
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-12-10 17:12:37 +0700

    upd create deposit

--- src/app/deposits/components/create-deposit-dialog/create-deposit-dialog.component.ts
+++ src/app/deposits/components/create-deposit-dialog/create-deposit-dialog.component.ts
@@ -1,17 +1,16 @@
-import { BehaviorSubject, of } from 'rxjs';
-import { first, map, switchMap } from 'rxjs/operators';
-import { Overwrite } from 'utility-types';
+import { BehaviorSubject, combineLatest, first, map, of, switchMap } from 'rxjs';
 
 import { Component, DestroyRef, TemplateRef, ViewChild, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, Validators } from '@angular/forms';
 
 import { DepositParams } from '@vality/fistful-proto/deposit';
-import { DialogSuperclass, NotifyLogService, progressTo } from '@vality/matez';
+import { DialogSuperclass, NotifyLogService, progressTo, tapLog } from '@vality/matez';
 import { ThriftFormExtension, isTypeWithAliases } from '@vality/ng-thrift';
 
-import { ThriftDepositManagementService } from '~/api/services';
-import { SourceCash } from '~/components/source-cash-field';
+import { DomainObjectsStoreService } from '~/api/domain-config';
+import { ThriftDepositManagementService, ThriftRepositoryClientService } from '~/api/services';
+import { getDomainObjectOption } from '~/components/thrift-api-crud/domain/services/domain-metadata-form-extensions/utils/get-domain-object-option';
 import { UserInfoBasedIdGeneratorService } from '~/services';
 
 import { FetchSourcesService } from '../../../sources';
@@ -21,13 +20,15 @@ import { FetchSourcesService } from '../../../sources';
     standalone: false,
 })
 export class CreateDepositDialogComponent extends DialogSuperclass<CreateDepositDialogComponent> {
-    private destroyRef = inject(DestroyRef);
+    private dr = inject(DestroyRef);
     private depositManagementService = inject(ThriftDepositManagementService);
     private log = inject(NotifyLogService);
     private userInfoBasedIdGeneratorService = inject(UserInfoBasedIdGeneratorService);
+    private domainStoreService = inject(DomainObjectsStoreService);
     private fetchSourcesService = inject(FetchSourcesService);
+    private thriftRepositoryClientService = inject(ThriftRepositoryClientService);
 
-    @ViewChild('sourceCashTemplate') sourceCashTemplate: TemplateRef<unknown>;
+    @ViewChild('currencyTemplate') currencyTemplate: TemplateRef<unknown>;
 
     control = new FormControl(this.getDefaultValue(), [Validators.required]);
     progress$ = new BehaviorSubject(0);
@@ -37,40 +38,59 @@ export class CreateDepositDialogComponent extends DialogSuperclass<CreateDeposit
                 of(
                     isTypeWithAliases(data, 'ContextSet', 'context') ||
                         isTypeWithAliases(data, 'DepositID', 'deposit') ||
-                        isTypeWithAliases(data, 'SourceID', 'deposit'),
+                        isTypeWithAliases(data, 'SourceID', 'deposit') ||
+                        isTypeWithAliases(data, 'PartyID', 'deposit'),
                 ),
             extension: () => of({ hidden: true }),
         },
         {
-            determinant: (data) => of(isTypeWithAliases(data, 'Cash', 'base')),
-            extension: () => of({ template: this.sourceCashTemplate }),
+            determinant: (data) => of(isTypeWithAliases(data, 'CurrencySymbolicCode', 'base')),
+            extension: () =>
+                this.fetchSourcesService.sources$.pipe(
+                    map((sources) => ({
+                        options: sources.map((source) => ({
+                            label: source.name,
+                            value: source.id,
+                            details: source,
+                        })),
+                        isIdentifier: true,
+                    })),
+                ),
+        },
+        {
+            determinant: (data) => of(isTypeWithAliases(data, 'WalletID', 'deposit')),
+            extension: () =>
+                this.domainStoreService.getLimitedObjects('wallet_config').value$.pipe(
+                    map((objects) => ({
+                        options: objects.map((obj) => getDomainObjectOption(obj)),
+                        isIdentifier: true,
+                    })),
+                ),
         },
     ];
 
     create() {
-        const { body: sourceCash, ...value } = this.control.value;
-        this.fetchSourcesService.sources$
+        const value = this.control.value;
+        combineLatest([
+            this.fetchSourcesService.sources$.pipe(first()),
+            this.thriftRepositoryClientService.CheckoutObject(
+                { head: {} },
+                { wallet_config: { id: value.wallet_id } },
+            ),
+        ])
             .pipe(
-                first(),
                 map(
-                    (sources) =>
-                        sources.find((s) => s.id === sourceCash.sourceId).currency_symbolic_code,
+                    ([sources, wallet]): DepositParams => ({
+                        ...value,
+                        party_id: wallet.object.wallet_config.data.party_ref.id,
+                        source_id: sources.find((s) => s.id === value.body.currency.symbolic_code)
+                            ?.id,
+                    }),
                 ),
-                switchMap((symbolicCode) =>
-                    this.depositManagementService.Create(
-                        {
-                            ...value,
-                            source_id: sourceCash.sourceId,
-                            body: {
-                                amount: sourceCash.amount,
-                                currency: { symbolic_code: symbolicCode },
-                            },
-                        },
-                        new Map(),
-                    ),
-                ),
+                tapLog(),
+                switchMap((params) => this.depositManagementService.Create(params, new Map())),
                 progressTo(this.progress$),
-                takeUntilDestroyed(this.destroyRef),
+                takeUntilDestroyed(this.dr),
             )
             .subscribe({
                 next: () => this.closeWithSuccess(),
@@ -81,7 +101,8 @@ export class CreateDepositDialogComponent extends DialogSuperclass<CreateDeposit
     getDefaultValue() {
         return {
             id: this.userInfoBasedIdGeneratorService.getUsernameBasedId(),
-            source_id: 'STUB',
-        } as Overwrite<DepositParams, { body: SourceCash }>;
+            source_id: '___AUTO___',
+            party_id: '___AUTO___',
+        } as DepositParams;
     }
 }
