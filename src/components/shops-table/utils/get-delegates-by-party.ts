50512daebd2f454490dcafb6d996787dade97c52
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-12-28 22:26:44 +0700

    ref

---
+++ src/components/shops-table/utils/get-delegates-by-party.ts
@@ -0,0 +1,38 @@
+import { Observable, combineLatest, map, of } from 'rxjs';
+
+import { inject } from '@angular/core';
+
+import {
+    DelegateWithPaymentInstitution,
+    PartyDelegateRulesetsService,
+} from '../../../app/parties/party/routing-rules-old/party-delegate-rulesets';
+import { RoutingRulesType } from '../../../app/parties/party/routing-rules-old/types/routing-rules-type';
+
+export function getDelegatesByParty(
+    parties: string[],
+    type: RoutingRulesType,
+): Observable<{
+    delegatesWithPaymentInstitutionByParty: Map<string, DelegateWithPaymentInstitution[]>;
+    rulesetIds: number[];
+}> {
+    const partyDelegateRulesetsService = inject(PartyDelegateRulesetsService);
+    return (
+        parties?.length
+            ? combineLatest(
+                  parties.map((id) =>
+                      partyDelegateRulesetsService.getDelegatesWithPaymentInstitution(type, id),
+                  ),
+              ).pipe(map((rules) => new Map(rules.map((r, idx) => [parties[idx], r]))))
+            : of(new Map<string, DelegateWithPaymentInstitution[]>())
+    ).pipe(
+        map((delegatesWithPaymentInstitutionByParty) => ({
+            delegatesWithPaymentInstitutionByParty,
+            rulesetIds: Array.from(
+                Array.from(delegatesWithPaymentInstitutionByParty.values()).reduce((acc, d) => {
+                    d?.map((v) => v?.partyDelegate?.ruleset?.id).forEach((v) => acc.add(v));
+                    return acc;
+                }, new Set<number>([])),
+            ),
+        })),
+    );
+}
