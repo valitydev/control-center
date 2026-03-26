4c66b7f1c8d581c9f15e390791d0d1d00485e541
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2026-03-26 21:34:58 +0400

    fix

--- projects/ng-thrift/src/lib/components/thrift-viewer/thrift-viewer.module.ts
+++ projects/ng-thrift/src/lib/components/thrift-viewer/thrift-viewer.module.ts
@@ -1,3 +1,5 @@
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
+
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,7 +8,6 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 
 import { ContentLoadingComponent } from '@vality/matez';
-import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 import { ThriftMonacoComponent } from '../thrift-monaco';
 
