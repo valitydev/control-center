1f17b5e32b56fa15f8d5cedc3fd833c92fb8acbb
Ray <11846445+A77AY@users.noreply.github.com>
2026-07-27 18:10:46 +0400

    XYZ-232: Add Sentry (#506)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import * as Sentry from '@sentry/angular';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+if (SENTRY_DSN) {
+    Sentry.init({
+        dsn: SENTRY_DSN,
+        integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+        tracesSampleRate: 1,
+        replaysSessionSampleRate: 1,
+        replaysOnErrorSampleRate: 1,
+        enableLogs: true,
+    });
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
