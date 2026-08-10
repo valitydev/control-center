09518b350426a50eb870a196c974c03fe4ec8e5f
Ray <11846445+A77AY@users.noreply.github.com>
2026-08-10 13:20:26 +0800

    refactor: replace console.info with console.dir for improved logging output

--- src/utils/thrift/provide-thrift-services.ts
+++ src/utils/thrift/provide-thrift-services.ts
@@ -7,7 +7,6 @@ import { Type, inject, isDevMode, makeEnvironmentProviders } from '@angular/core
 import * as Sentry from '@sentry/angular';
 
 import { ConnectOptions } from '@vality/domain-proto';
-import { toJson } from '@vality/ng-thrift';
 
 import { ConfigService, KeycloakUserService } from '~/services';
 
@@ -117,9 +116,9 @@ const logger: ConnectOptions['loggingFn'] = (params) => {
             console.error(parsedError.error);
             if (LOGGING.fullLogging) {
                 console.log('Arguments');
-                console.log(JSON.stringify(toJson(params.args), null, 2));
+                console.dir(params.args);
                 console.log('Headers');
-                console.log(params.headers);
+                console.dir(params.headers);
             }
             console.groupEnd();
             return;
@@ -128,11 +127,11 @@ const logger: ConnectOptions['loggingFn'] = (params) => {
             if (LOGGING.fullLogging) {
                 console.groupCollapsed(`🟢\u00A0${info}`);
                 console.log('Arguments');
-                console.log(JSON.stringify(toJson(params.args), null, 2));
+                console.dir(params.args);
                 console.log('Response');
-                console.log(JSON.stringify(toJson(params.response), null, 2));
+                console.dir(params.response);
                 console.log('Headers');
-                console.log(params.headers);
+                console.dir(params.headers);
                 console.groupEnd();
             }
             return;
